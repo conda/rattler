392ae9a750a594002d871467f198ab73afce41d5
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-02-16 11:42:07 +0100

    Update crates/rattler_conda_types/src/run_export.rs
    Co-authored-by: Bas Zalmstra <zalmstra.bas@gmail.com>

--- crates/rattler_conda_types/src/run_export.rs
+++ crates/rattler_conda_types/src/run_export.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 /// Type of runtime export.
 /// See more info in [the conda docs](https://docs.conda.io/projects/conda-build/en/latest/resources/define-metadata.html#export-runtime-requirements)
-/// [`package::run_exports::RunExports`]
+/// [`crate::package::RunExports`]
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub enum RunExportKind {
     Weak,
