c9eb22c9068ea384c163e7e797202c3501688819
Adolfo Ochagavía <github@adolfo.ochagavia.nl>
2023-02-20 14:05:06 +0100

    Add missing docs and #![deny(missing_docs)]

--- crates/rattler_solve/src/lib.rs
+++ crates/rattler_solve/src/lib.rs
@@ -1,3 +1,8 @@
+#![deny(missing_docs)]
+
+//! `rattler_solve` is a crate that provides functionality to solve Conda environments. It currently
+//! exposes the functionality through the [`SolverProblem::solve`] function.
+
 mod libsolv;
 mod package_operation;
 
@@ -5,31 +10,59 @@ pub use package_operation::{PackageIdentifier, PackageOperation, PackageOperatio
 
 use rattler_conda_types::{MatchSpec, RepoData};
 
+/// Represents an error when solving the dependencies for a given environment
 #[derive(thiserror::Error, Debug)]
 pub enum SolveError {
+    /// There is no set of dependencies that satisfies the requirements
     #[error("unsolvable")]
     Unsolvable,
 
+    /// An error occurred when trying to load the channel and platform's `repodata.json`
     #[error("error adding repodata: {0}")]
     ErrorAddingRepodata(#[source] anyhow::Error),
 
+    /// An error occurred when trying to load information about installed packages to the solver
     #[error("error adding installed packages: {0}")]
     ErrorAddingInstalledPackages(#[source] anyhow::Error),
 
+    /// The solver backend returned operations that have no known mapping to [`PackageOperationKind`]
     #[error("unsupported operations")]
     UnsupportedOperations,
 }
 
+/// Represents known information about a single package that is already installed in the Conda
+/// environment
 #[derive(Debug)]
 pub struct InstalledPackage {
+    /// The package's name
     pub name: String,
+    /// The package's version
     pub version: String,
 
     // TODO: does it make sense to include the following data?
+    /// The package's build string, if present
     pub build_string: Option<String>,
+    /// The package's build number, if present
     pub build_number: Option<usize>,
 }
 
+/// Represents the action that we want to perform on a given package, so the solver can take it into
+/// account (e.g. specifying [`RequestedAction::Install`] for a package that has already been
+/// installed will result in no operations, but specifying [`RequestedAction::Update`] will generate
+/// the necessary operations to update the package to a newer version if it exists and the update is
+/// compatible with the rest of the environment).
+#[derive(Debug, Copy, Clone)]
+pub enum RequestedAction {
+    /// The package is being installed
+    Install,
+    /// The package is being removed
+    Remove,
+    /// The package is being updated
+    Update,
+}
+
+/// Represents a dependency resolution problem, to be solved by one of the backends (currently only
+/// libsolv is supported)
 #[derive(Debug)]
 pub struct SolverProblem<'c> {
     /// All the available channels (and contents) in order of priority
@@ -42,14 +75,9 @@ pub struct SolverProblem<'c> {
     pub specs: Vec<(MatchSpec, RequestedAction)>,
 }
 
-#[derive(Debug, Copy, Clone)]
-pub enum RequestedAction {
-    Install,
-    Remove,
-    Update,
-}
-
 impl<'c> SolverProblem<'c> {
+    /// Resolve the dependencies and return the required [`PackageOperation`]s in the order in which
+    /// they need to be applied
     pub fn solve(self) -> Result<Vec<PackageOperation>, SolveError> {
         // TODO: support other backends, such as https://github.com/pubgrub-rs/pubgrub
         libsolv::solve(self)
--- crates/rattler_solve/src/package_operation.rs
+++ crates/rattler_solve/src/package_operation.rs
@@ -1,24 +1,37 @@
-/// Enumeration of all operations on packages
+/// Represents an operation that needs to be applied on a package, in order to bring the Conda
+/// environment to the desired state
 #[derive(Debug, Clone)]
 pub struct PackageOperation {
+    /// The package to be operated on
     pub package: PackageIdentifier,
+    /// The required operation kind
     pub kind: PackageOperationKind,
 }
 
-/// A package identifier, used for deciding what to install/remove and where
+/// Represents a package (installed or not) that will be modified
 #[derive(Debug, Clone, serde::Serialize)]
 pub struct PackageIdentifier {
+    /// The package's name
     pub name: String,
+    /// The package's version
     pub version: String,
+    /// The package's location
     pub location: Option<String>,
+    /// The package's channel
     pub channel: String,
+    /// The package's build string, if known
     pub build_string: Option<String>,
+    /// The package's build number, if known
     pub build_number: Option<usize>,
 }
 
+/// Represents the operations that rattler supports on packages
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum PackageOperationKind {
+    /// The package should be installed
     Install,
+    /// The package should be removed
     Remove,
+    /// The package should be reinstalled
     Reinstall,
 }
