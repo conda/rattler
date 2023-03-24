061fae753ef2dbe64e72994e0d0760f0163fce6f
Bas Zalmstra <bas@prefix.dev>
2023-03-24 10:45:16 +0100

    feat: generate bindings statically

--- crates/rattler_solve/build.rs
+++ crates/rattler_solve/build.rs
@@ -1,26 +1,5 @@
 use anyhow::{anyhow, Result};
-use std::{
-    fs,
-    path::{self, Path, PathBuf},
-};
-
-const ALLOWED_FUNC_PREFIX: &[&str] = &[
-    "map",
-    "policy",
-    "pool",
-    "prune",
-    "queue",
-    "repo",
-    "repodata",
-    "selection",
-    "solv",
-    "solver",
-    "testcase",
-    "transaction",
-    "dataiterator",
-    "datamatcher",
-    "stringpool",
-];
+use std::path::{self, PathBuf};
 
 fn build_libsolv() -> Result<PathBuf> {
     let target = std::env::var("TARGET").unwrap();
@@ -61,50 +40,8 @@ fn build_libsolv() -> Result<PathBuf> {
     Ok(out.join("include/solv"))
 }
 
-fn check_solvext_bindings(
-    include_path: &Path,
-    builder: bindgen::Builder,
-) -> Result<bindgen::Builder> {
-    let mut builder = builder;
-    for inc in fs::read_dir(include_path)? {
-        let inc = inc?;
-        let name = inc.file_name();
-        let name = name.to_string_lossy();
-        // all the solvext include files are named like `repo_<format_name>.h`
-        if name.starts_with("repo_") && name.ends_with(".h") {
-            dbg!(inc.path());
-            builder = builder.header(inc.path().to_str().unwrap());
-        }
-    }
-
-    Ok(builder)
-}
-
-fn generate_bindings(include_path: &Path) -> Result<()> {
-    let output = std::env::var("OUT_DIR")?;
-    let generator = bindgen::Builder::default()
-        .header(include_path.join("solver.h").to_str().unwrap())
-        .header(include_path.join("solverdebug.h").to_str().unwrap())
-        .header(include_path.join("queue.h").to_str().unwrap())
-        .header(include_path.join("pool.h").to_str().unwrap())
-        .header(include_path.join("selection.h").to_str().unwrap())
-        .header(include_path.join("knownid.h").to_str().unwrap())
-        .header(include_path.join("conda.h").to_str().unwrap())
-        .header(include_path.join("repo_conda.h").to_str().unwrap())
-        .allowlist_type("(Id|solv_knownid)")
-        .allowlist_var(".*")
-        .allowlist_function(format!("({}).*", ALLOWED_FUNC_PREFIX.join("|")));
-    check_solvext_bindings(include_path, generator)?
-        .generate()
-        .unwrap()
-        .write_to_file(Path::new(&output).join("libsolv_bindings.rs"))?;
-
-    Ok(())
-}
-
 fn main() -> Result<()> {
-    let include_path = build_libsolv()?;
-    generate_bindings(&include_path)?;
+    build_libsolv()?;
 
     Ok(())
 }
---
+++ crates/tools/src/libsolv_bindings.rs
@@ -0,0 +1,81 @@
+use crate::{project_root, reformat, update, Mode};
+use tempdir::TempDir;
+
+const ALLOWED_FUNC_PREFIX: &[&str] = &[
+    "map",
+    "policy",
+    "pool",
+    "prune",
+    "queue",
+    "repo",
+    "repodata",
+    "selection",
+    "solv",
+    "solver",
+    "testcase",
+    "transaction",
+    "dataiterator",
+    "datamatcher",
+    "stringpool",
+];
+
+/// Generate or verify the libsolv bindings.
+pub fn generate(mode: Mode) -> anyhow::Result<()> {
+    let libsolv_path = project_root().join("crates/rattler_solve/libsolv");
+
+    // Normally the `solvversion.h` is generated from the `solverversion.h.in` by CMake when
+    // building libsolv. However, for the bindings we don't need that much information from that
+    // file. So to be able to generate proper bindings we use a drop-in-replacement for this file
+    // that contains just the macros that are needed for the bindings.
+    //
+    // The behavior of this file might change when we update libsolv so its important to check this
+    // with every upgrade.
+    let temp_include_dir = TempDir::new("libsolv")?;
+    std::fs::write(
+        temp_include_dir.path().join("solvversion.h"),
+        r#"#ifndef LIBSOLV_SOLVVERSION_H
+#define LIBSOLV_SOLVVERSION_H
+
+#define SOLV_API
+
+#define LIBSOLV_FEATURE_MULTI_SEMANTICS
+#define LIBSOLV_FEATURE_CONDA
+
+#define LIBSOLVEXT_FEATURE_ZLIB_COMPRESSION
+
+#endif
+"#,
+    )?;
+
+    // Define the contents of the bindings and how they are generated
+    let bindings = bindgen::Builder::default()
+        .clang_arg(format!("-I{}", temp_include_dir.path().display()))
+        .header(libsolv_path.join("src/solver.h").to_str().unwrap())
+        .header(libsolv_path.join("src/solverdebug.h").to_str().unwrap())
+        .header(libsolv_path.join("src/queue.h").to_str().unwrap())
+        .header(libsolv_path.join("src/pool.h").to_str().unwrap())
+        .header(libsolv_path.join("src/selection.h").to_str().unwrap())
+        .header(libsolv_path.join("src/knownid.h").to_str().unwrap())
+        .header(libsolv_path.join("src/conda.h").to_str().unwrap())
+        .header(libsolv_path.join("src/repo_solv.h").to_str().unwrap())
+        .header(libsolv_path.join("src/repo_write.h").to_str().unwrap())
+        .header(libsolv_path.join("ext/repo_conda.h").to_str().unwrap())
+        .allowlist_type("(Id|solv_knownid)")
+        .allowlist_var(".*")
+        .allowlist_function(format!("({}).*", ALLOWED_FUNC_PREFIX.join("|")))
+        .generate()?;
+
+    // Generate the actual bindings and format them
+    let mut libsolv_bindings = Vec::new();
+    bindings.write(Box::new(&mut libsolv_bindings))?;
+
+    // Add a preemble to the bindings to ensure clippy also passes.
+    let libsolv_bindings = reformat(format!("#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, dead_code, clippy::upper_case_acronyms)]\n\n{}", String::from_utf8(libsolv_bindings).unwrap()))?;
+
+    // Write (or check) the bindings
+    update(
+        &project_root().join("crates/rattler_solve/src/libsolv/wrapper/ffi.rs"),
+        &libsolv_bindings,
+        mode,
+    )
+}
