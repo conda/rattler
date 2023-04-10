f28423e140b31737df5f2afa2d24e32cb1057260
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-04-10 10:25:32 +0200

    change interface to use url directly and use Either for the stream

--- crates/rattler_package_streaming/src/reqwest/tokio.rs
+++ crates/rattler_package_streaming/src/reqwest/tokio.rs
@@ -4,10 +4,37 @@
 use crate::ExtractError;
 use futures_util::stream::TryStreamExt;
 use rattler_conda_types::package::ArchiveType;
-use reqwest::{Client, IntoUrl, Response};
+use reqwest::{Client, Response};
 use std::path::Path;
 use tokio::io::BufReader;
+use tokio_util::either::Either;
 use tokio_util::io::StreamReader;
+use url::Url;
+
+async fn get_reader(url: Url, client: Client) -> Result<impl tokio::io::AsyncRead, ExtractError> {
+    if url.scheme() == "file" {
+        let file = tokio::fs::File::open(url.to_file_path().expect("..."))
+            .await
+            .map_err(ExtractError::IoError)?;
+
+        Ok(Either::Left(BufReader::new(file)))
+    } else {
+        // Send the request for the file
+        let response = client
+            .get(url.clone())
+            .send()
+            .await
+            .and_then(Response::error_for_status)
+            .map_err(ExtractError::ReqwestError)?;
+
+        // Get the response as a stream
+        Ok(Either::Right(StreamReader::new(
+            response
+                .bytes_stream()
+                .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err)),
+        )))
+    }
+}
 
 /// Extracts the contents a `.tar.bz2` package archive from the specified remote location.
 ///
@@ -19,16 +46,18 @@ use tokio_util::io::StreamReader;
 /// # use reqwest::Client;
 /// let _ = extract_tar_bz2(
 ///     Client::default(),
-///     "https://conda.anaconda.org/conda-forge/win-64/python-3.11.0-hcf16a7b_0_cpython.tar.bz2",
+///     Url::parse("https://conda.anaconda.org/conda-forge/win-64/python-3.11.0-hcf16a7b_0_cpython.tar.bz2").unwrap(),
 ///     Path::new("/tmp"))
 ///     .await
 ///     .unwrap();
 /// # }
 /// ```
 pub async fn extract_tar_bz2(
-    reader: impl tokio::io::AsyncRead + Send + 'static,
+    client: Client,
+    url: Url,
     destination: &Path,
 ) -> Result<(), ExtractError> {
+    let reader = get_reader(url.clone(), client).await?;
     // The `response` is used to stream in the package data
     crate::tokio::async_read::extract_tar_bz2(reader, destination).await
 }
@@ -40,20 +69,23 @@ pub async fn extract_tar_bz2(
 /// # async fn main() {
 /// # use std::path::Path;
 /// use rattler_package_streaming::reqwest::tokio::extract_conda;
+/// use url::Url;
 /// # use reqwest::Client;
 /// let _ = extract_conda(
 ///     Client::default(),
-///     "https://conda.anaconda.org/conda-forge/linux-64/python-3.10.8-h4a9ceb5_0_cpython.conda",
+///     Url::parse("https://conda.anaconda.org/conda-forge/linux-64/python-3.10.8-h4a9ceb5_0_cpython.conda").unwrap(),
 ///     Path::new("/tmp"))
 ///     .await
 ///     .unwrap();
 /// # }
 /// ```
 pub async fn extract_conda(
-    reader: impl tokio::io::AsyncRead + Send + 'static,
+    client: Client,
+    url: Url,
     destination: &Path,
 ) -> Result<(), ExtractError> {
     // The `response` is used to stream in the package data
+    let reader = get_reader(url.clone(), client).await?;
     crate::tokio::async_read::extract_conda(reader, destination).await
 }
 
@@ -68,64 +100,17 @@ pub async fn extract_conda(
 /// # use reqwest::Client;
 /// let _ = extract(
 ///     Client::default(),
-///     "https://conda.anaconda.org/conda-forge/linux-64/python-3.10.8-h4a9ceb5_0_cpython.conda",
+///     Url::parse("https://conda.anaconda.org/conda-forge/linux-64/python-3.10.8-h4a9ceb5_0_cpython.conda").unwrap(),
 ///     Path::new("/tmp"))
 ///     .await
 ///     .unwrap();
 /// # }
 /// ```
-pub async fn extract(
-    client: Client,
-    url: impl IntoUrl,
-    destination: &Path,
-) -> Result<(), ExtractError> {
-    let url = url
-        .into_url()
-        .map_err(reqwest::Error::from)
-        .map_err(ExtractError::ReqwestError)?;
-
-    if url.scheme() == "file" {
-        let file = tokio::fs::File::open(url.to_file_path().expect("..."))
-            .await
-            .map_err(ExtractError::IoError)?;
-
-        let stream_reader = BufReader::new(file);
-
-        match ArchiveType::try_from(Path::new(url.path()))
-            .ok_or(ExtractError::UnsupportedArchiveType)?
-        {
-            ArchiveType::TarBz2 => {
-                crate::tokio::async_read::extract_tar_bz2(stream_reader, destination).await
-            }
-            ArchiveType::Conda => {
-                crate::tokio::async_read::extract_conda(stream_reader, destination).await
-            }
-        }
-    } else {
-        // Send the request for the file
-        let response = client
-            .get(url.clone())
-            .send()
-            .await
-            .and_then(Response::error_for_status)
-            .map_err(ExtractError::ReqwestError)?;
-
-        // Get the response as a stream
-        let reader = StreamReader::new(
-            response
-                .bytes_stream()
-                .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err)),
-        );
-
-        match ArchiveType::try_from(Path::new(url.path()))
-            .ok_or(ExtractError::UnsupportedArchiveType)?
-        {
-            ArchiveType::TarBz2 => {
-                crate::tokio::async_read::extract_tar_bz2(reader, destination).await
-            }
-            ArchiveType::Conda => {
-                crate::tokio::async_read::extract_conda(reader, destination).await
-            }
-        }
+pub async fn extract(client: Client, url: Url, destination: &Path) -> Result<(), ExtractError> {
+    match ArchiveType::try_from(Path::new(url.path()))
+        .ok_or(ExtractError::UnsupportedArchiveType)?
+    {
+        ArchiveType::TarBz2 => extract_tar_bz2(client, url, destination).await,
+        ArchiveType::Conda => extract_conda(client, url, destination).await,
     }
 }
