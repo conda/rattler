aa3d68a3bfff2c4aed4f7b898ef534c3f86d763a
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-05-06 12:57:31 +0200

    ..

--- crates/rattler/src/install/mod.rs
+++ crates/rattler/src/install/mod.rs
@@ -4,10 +4,10 @@ pub mod link;
 mod python;
 mod transaction;
 
+pub use crate::install::entry_point::python_entry_point_template;
 pub use driver::InstallDriver;
 pub use link::{link_file, LinkFileError};
 pub use transaction::{Transaction, TransactionOperation};
-pub use crate::install::entry_point::python_entry_point_template;
 
 use crate::install::entry_point::{
     create_unix_python_entry_point, create_windows_python_entry_point,
