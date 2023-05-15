e2862e673dd1c74f9d19e0c3249413a4682941a4
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-05-15 11:44:59 +0200

    add some initial test

--- crates/rattler_networking/src/lib.rs
+++ crates/rattler_networking/src/lib.rs
@@ -1,18 +1,36 @@
+#![deny(missing_docs)]
+
+//! Networking utilities for Rattler, specifically authenticating requests
+
 use std::{collections::HashMap, str::FromStr};
 
 use keyring::Entry;
 use reqwest::{Client, IntoUrl, Method, Url};
 
-#[derive(Clone)]
+/// The different Authentication methods that are supported in the conda
+/// ecosystem
+#[derive(Clone, PartialEq, Eq)]
 pub enum Authentication {
+    /// A bearer token is sent as a header of the form
+    /// `Authorization: Bearer {TOKEN}`
     BearerToken(String),
-    Basic { username: String, password: String },
+    /// A basic authentication token is sent as HTTP basic auth
+    Basic {
+        /// The username to use for basic auth
+        username: String,
+        /// The password to use for basic auth
+        password: String,
+    },
+    /// A conda token is sent in the URL as `/t/{TOKEN}/...`
     CondaToken(String),
 }
 
+/// An error that can occur when parsing an authentication string
 #[derive(Debug)]
 pub enum AuthenticationParseError {
+    /// The scheme is not valid
     InvalidScheme,
+    /// The token could not be parsed
     InvalidToken,
 }
 
@@ -30,6 +48,7 @@ pub struct AuthenticationStorage {
 }
 
 impl AuthenticationStorage {
+    /// Create a new authentication storage with the given store key
     pub fn new(store_key: &str) -> AuthenticationStorage {
         AuthenticationStorage {
             store_key: store_key.to_string(),
@@ -41,6 +60,7 @@ impl AuthenticationStorage {
 impl FromStr for Authentication {
     type Err = AuthenticationParseError;
 
+    /// Parse an authentication string into an Authentication struct
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         let mut parts = s.split_whitespace();
         let scheme = parts.next().unwrap_or_default();
@@ -62,16 +82,23 @@ impl FromStr for Authentication {
     }
 }
 
+/// An error that can occur when accessing the authentication storage
 #[derive(thiserror::Error, Debug)]
 pub enum AuthenticationStorageError {
+    /// An error occurred when accessing the authentication storage
     #[error("Could not retrieve credentials from authentication storage: {0}")]
     StorageError(#[from] keyring::Error),
 
+    /// An error occurred when parsing the credentials
     #[error("Could not parse credentials stored for {host}")]
-    ParseCredentialsError { host: String },
+    ParseCredentialsError {
+        /// The host for which the credentials could not be parsed
+        host: String,
+    },
 }
 
 impl AuthenticationStorage {
+    /// Store the given authentication information for the given host
     pub fn store(&self, host: &str, authentication: &Authentication) -> keyring::Result<()> {
         let entry = Entry::new(&self.store_key, host)?;
         match authentication {
@@ -90,6 +117,7 @@ impl AuthenticationStorage {
         }
     }
 
+    /// Retrieve the authentication information for the given host
     pub fn get(&self, host: &str) -> Result<Option<Authentication>, AuthenticationStorageError> {
         if let Some(cached) = self.authentication_cache.get(host) {
             return Ok(cached.clone());
@@ -109,6 +137,7 @@ impl AuthenticationStorage {
         }
     }
 
+    /// Retrieve the authentication information for the given URL
     pub fn get_by_url<U: IntoUrl>(
         &self,
         url: U,
@@ -130,16 +159,20 @@ impl AuthenticationStorage {
         }
     }
 
+    /// Delete the authentication information for the given host
     pub fn delete(&self, host: &str) -> keyring::Result<()> {
         let entry = Entry::new(&self.store_key, host)?;
         entry.delete_password()
     }
 }
 
+/// A client that can be used to make authenticated requests, based on the [`reqwest::Client`]
 #[derive(Clone)]
 pub struct AuthenticatedClient {
+    /// The underlying client
     client: Client,
 
+    /// The authentication storage
     auth_storage: AuthenticationStorage,
 }
 
@@ -153,6 +186,7 @@ impl Default for AuthenticatedClient {
 }
 
 impl AuthenticatedClient {
+    /// Create a new authenticated client from the given client and authentication storage
     pub fn from_client(client: Client, auth_storage: AuthenticationStorage) -> AuthenticatedClient {
         AuthenticatedClient {
             client,
@@ -162,18 +196,22 @@ impl AuthenticatedClient {
 }
 
 impl AuthenticatedClient {
+    /// Create a GET request builder for the given URL (see also [`reqwest::Client::get`])
     pub fn get<U: IntoUrl>(&self, url: U) -> reqwest::RequestBuilder {
         self.request(Method::GET, url)
     }
 
+    /// Create a POST request builder for the given URL (see also [`reqwest::Client::post`])
     pub fn post<U: IntoUrl>(&self, url: U) -> reqwest::RequestBuilder {
         self.request(Method::POST, url)
     }
 
+    /// Create a HEAD request builder for the given URL (see also [`reqwest::Client::head`])
     pub fn head<U: IntoUrl>(&self, url: U) -> reqwest::RequestBuilder {
         self.request(Method::HEAD, url)
     }
 
+    /// Create a request builder for the given URL (see also [`reqwest::Client::request`])
     pub fn request<U: IntoUrl>(&self, method: Method, url: U) -> reqwest::RequestBuilder {
         let url_clone = url.as_str().to_string();
         match self.auth_storage.get_by_url(url) {
@@ -189,6 +227,7 @@ impl AuthenticatedClient {
         }
     }
 
+    /// Authenticate the given URL with the given authentication information
     fn authenticate_url(&self, url: Url, auth: &Option<Authentication>) -> Url {
         if let Some(credentials) = auth {
             match credentials {
@@ -208,6 +247,7 @@ impl AuthenticatedClient {
         }
     }
 
+    /// Authenticate the given request builder with the given authentication information
     fn authenticate_request(
         &self,
         builder: reqwest::RequestBuilder,
@@ -227,13 +267,17 @@ impl AuthenticatedClient {
     }
 }
 
+/// A blocking client that can be used to make authenticated requests, based on the [`reqwest::blocking::Client`]
 pub struct AuthenticatedClientBlocking {
+    /// The underlying client
     client: reqwest::blocking::Client,
 
+    /// The authentication storage
     auth_storage: AuthenticationStorage,
 }
 
 impl AuthenticatedClientBlocking {
+    /// Create a new authenticated client from the given client and authentication storage
     pub fn from_client(
         client: reqwest::blocking::Client,
         auth_storage: AuthenticationStorage,
@@ -255,18 +299,22 @@ impl Default for AuthenticatedClientBlocking {
 }
 
 impl AuthenticatedClientBlocking {
+    /// Create a GET request builder for the given URL (see also [`reqwest::blocking::Client::get`])
     pub fn get<U: IntoUrl>(&self, url: U) -> reqwest::blocking::RequestBuilder {
         self.request(Method::GET, url)
     }
 
+    /// Create a POST request builder for the given URL (see also [`reqwest::blocking::Client::post`])
     pub fn post<U: IntoUrl>(&self, url: U) -> reqwest::blocking::RequestBuilder {
         self.request(Method::POST, url)
     }
 
+    /// Create a HEAD request builder for the given URL (see also [`reqwest::blocking::Client::head`])
     pub fn head<U: IntoUrl>(&self, url: U) -> reqwest::blocking::RequestBuilder {
         self.request(Method::HEAD, url)
     }
 
+    /// Create a request builder for the given URL (see also [`reqwest::blocking::Client::request`])
     pub fn request<U: IntoUrl>(&self, method: Method, url: U) -> reqwest::blocking::RequestBuilder {
         let url_clone = url.as_str().to_string();
         match self.auth_storage.get_by_url(url) {
@@ -282,6 +330,7 @@ impl AuthenticatedClientBlocking {
         }
     }
 
+    /// Authenticate the given URL with the given authentication information
     fn authenticate_url(&self, url: Url, auth: &Option<Authentication>) -> Url {
         if let Some(credentials) = auth {
             match credentials {
@@ -301,6 +350,7 @@ impl AuthenticatedClientBlocking {
         }
     }
 
+    /// Authenticate the given request builder with the given authentication information
     fn authenticate_request(
         &self,
         builder: reqwest::blocking::RequestBuilder,
@@ -317,5 +367,38 @@ impl AuthenticatedClientBlocking {
         } else {
             builder
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_storage() -> anyhow::Result<()> {
+        let storage = super::AuthenticationStorage::new("rattler_test");
+        let host = "test.example.com";
+        let retrieved = storage.get(host);
+        assert!(retrieved.is_ok());
+        assert!(retrieved.unwrap().is_none());
+
+        let authentication = Authentication::CondaToken("testtoken".to_string());
+        storage.store(host, &authentication)?;
+
+        let retrieved = storage.get(host);
+        assert!(retrieved.is_ok());
+        let retrieved = retrieved.unwrap();
+        assert!(retrieved.is_some());
+        let auth = retrieved.unwrap();
+        assert!(auth == authentication);
+
+        let client = AuthenticatedClient::from_client(reqwest::Client::default(), storage.clone());
+        let request = client.get("https://test.example.com/conda-forge/noarch/testpkg.tar.bz2");
+        let request = request.build().unwrap();
+        let url = request.url();
+        assert!(url.path().starts_with("/t/testtoken"));
+
+        storage.delete(host)?;
+        Ok(())
     }
 }
