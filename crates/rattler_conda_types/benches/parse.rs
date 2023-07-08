71fd3af002747ffc0b30d9f6d045a1037e21f311
Bas Zalmstra <bas@prefix.dev>
2023-07-08 22:08:35 +0200

    feat: add benchmarks

--- crates/rattler_conda_types/benches/parse.rs
+++ crates/rattler_conda_types/benches/parse.rs
@@ -8,6 +8,18 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("parse complex version", |b| {
         b.iter(|| black_box("1!1.0b2.post345.dev456+3.2.20.rc3").parse::<Version>())
     });
+    c.bench_function("parse logical constraint", |b| {
+        b.iter(|| black_box(">=3.1").parse::<Version>())
+    });
+    c.bench_function("parse wildcard constraint", |b| {
+        b.iter(|| black_box("3.1.*").parse::<Version>())
+    });
+    c.bench_function("parse simple version spec", |b| {
+        b.iter(|| black_box(">=3.1").parse::<Version>())
+    });
+    c.bench_function("parse complex version spec", |b| {
+        b.iter(|| black_box("(>=2.1.0,<3.0)|(~=3.2.1,~3.2.2.1)|(==4.1)").parse::<Version>())
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
