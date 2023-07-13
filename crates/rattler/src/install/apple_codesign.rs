7ee16d8dc6d4bce1e79bd0a209d0cd3fa2569ca2
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-07-13 14:10:58 +0200

    call codesign binary instead of using codesign crate (#259)

---
+++ crates/rattler/src/install/apple_codesign.rs
@@ -0,0 +1,34 @@
+use super::LinkFileError;
+use std::path::Path;
+
+/// Controls the behavior of the [`super::link_package`] function when it encounters a binary that needs
+/// to be signed on macOS ARM64 (Apple Silicon).
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub enum AppleCodeSignBehavior {
+    /// Do nothing (do not attempt to sign any binary)
+    DoNothing,
+    /// Ignore if the signing fails
+    Ignore,
+    /// Bubble up the error if the code signing fails (default)
+    #[default]
+    Fail,
+}
+
+/// Sign a binary using the `codesign` tool with an ad-hoc certificate on  macOS.
+/// This is required for binaries to run on Apple Silicon.
+pub(crate) fn codesign(destination_path: &Path) -> Result<(), LinkFileError> {
+    let status = std::process::Command::new("/usr/bin/codesign")
+        .arg("--sign")
+        // Use an ad-hoc certificate (`-`)
+        .arg("-")
+        // replace any existing signature
+        .arg("--force")
+        .arg(destination_path)
+        .status()?;
+
+    if !status.success() {
+        return Err(LinkFileError::FailedToSignAppleBinary);
+    }
+
+    Ok(())
+}
