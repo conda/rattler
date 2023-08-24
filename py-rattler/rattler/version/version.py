7e229b0dc5a2e5044a08e9e038783dc1cc3675f0
Wackyator <pratapsinghtarun9@gmail.com>
2023-08-24 14:03:24 +0530

    fix: make Version property names more explicit and raise exceptions instead of implicitly returning None

--- py-rattler/rattler/version/version.py
+++ py-rattler/rattler/version/version.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Optional, Tuple
 
-from rattler.rattler import PyVersion
+from rattler.rattler import PyVersion, InvalidVersionError
 
 
 class Version:
@@ -55,7 +55,7 @@ class Version:
         return Version._from_py_version(self._version.bump())
 
     @property
-    def local(self) -> bool:
+    def has_local(self) -> bool:
         """
         Returns true if this version has a local segment defined.
 
@@ -80,7 +80,7 @@ class Version:
         return self._version.as_major_minor()
 
     @property
-    def dev(self) -> bool:
+    def is_dev(self) -> bool:
         """
         Returns true if the version contains a component name "dev".
 
@@ -112,7 +112,7 @@ class Version:
         """
         return self._version.compatible_with(other._version)
 
-    def pop_segments(self, n: int = 1) -> Optional[Version]:
+    def pop_segments(self, n: int = 1) -> Version:
         """
         Pops `n` number of segments from the version and returns
         the new version. Returns `None` if the version becomes
@@ -120,10 +120,11 @@ class Version:
         """
         new_py_version = self._version.pop_segments(n)
         if new_py_version:
-            # maybe it should raise an exception instead?
             return self._from_py_version(new_py_version)
+        else:
+            raise InvalidVersionError("Version must have atleast 1 valid segment")
 
-    def with_segments(self, start: int, stop: int) -> Optional[Version]:
+    def with_segments(self, start: int, stop: int) -> Version:
         """
         Returns new version with with segments ranging from `start` to `stop`.
         `stop` is exclusive.
@@ -132,9 +133,9 @@ class Version:
         if new_py_version:
             return self._from_py_version(new_py_version)
         else:
-            # maybe it should raise an exception instead?
-            return None
+            raise InvalidVersionError("Invalid segment range provided")
 
+    @property
     def segment_count(self) -> int:
         """
         Returns the number of segments in the version.
