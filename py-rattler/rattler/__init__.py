563fafbb3600afb920ebf806913a5d5c40d68d2d
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-09-06 13:02:21 +0200

    feat: add channel types to py-rattler (#313)

--- py-rattler/rattler/__init__.py
+++ py-rattler/rattler/__init__.py
@@ -1,5 +1,13 @@
 from rattler.version import Version
 from rattler.match_spec import MatchSpec, NamelessMatchSpec
 from rattler.repo_data import PackageRecord
+from rattler.channel import Channel, ChannelConfig
 
-__all__ = ["Version", "MatchSpec", "NamelessMatchSpec", "PackageRecord"]
+__all__ = [
+    "Version",
+    "MatchSpec",
+    "NamelessMatchSpec",
+    "PackageRecord",
+    "Channel",
+    "ChannelConfig",
+]
