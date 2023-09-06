f805cc606b195af4321fdb76e3b1b099b90f295e
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-09-06 11:48:40 +0200

    fix all tests

--- py-rattler/rattler/channel/channel.py
+++ py-rattler/rattler/channel/channel.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional, TYPE_CHECKING
+from typing import Optional
 
 from rattler.rattler import PyChannel
 from rattler.channel.channel_config import ChannelConfig
