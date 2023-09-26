9b078699b0484025d550ddfee4475b355484ebec
Wackyator <pratapsinghtarun9@gmail.com>
2023-09-26 13:02:17 +0530

    test: add test-server license

--- test-data/test-server/reposerver.py
+++ test-data/test-server/reposerver.py
@@ -1,3 +1,6 @@
+# File taken from https://github.com/mamba-org/mamba/tree/a8d595b6ff8ac182e60741c3e8cbd142e7d19905/mamba/tests
+# under BSD-3-Clause license
+
 import argparse
 import base64
 import glob
