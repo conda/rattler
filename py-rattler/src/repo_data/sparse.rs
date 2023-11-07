72e9af97b4dd6f1f8a4cabb54a4700d704c7ceab
Ruben Arts <ruben.arts@hotmail.com>
2023-11-07 16:19:02 +0100

    fmt

--- py-rattler/src/repo_data/sparse.rs
+++ py-rattler/src/repo_data/sparse.rs
@@ -66,14 +66,12 @@ impl PySparseRepoData {
         py.allow_threads(move || {
             let repo_data = repo_data.iter().map(Into::into);
             let package_names = package_names.into_iter().map(Into::into);
-            Ok(SparseRepoData::load_records_recursive(
-                repo_data,
-                package_names,
-                None,
-            )?
-            .into_iter()
-            .map(|v| v.into_iter().map(Into::into).collect::<Vec<_>>())
-            .collect::<Vec<_>>())
+            Ok(
+                SparseRepoData::load_records_recursive(repo_data, package_names, None)?
+                    .into_iter()
+                    .map(|v| v.into_iter().map(Into::into).collect::<Vec<_>>())
+                    .collect::<Vec<_>>(),
+            )
         })
     }
 }
