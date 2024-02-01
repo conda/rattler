ba1b425b66540486f04d8eec6bcc6fd1a0c263a3
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-02-01 11:13:57 +0100

    update most dependencies

--- crates/rattler_repodata_gateway/src/fetch/mod.rs
+++ crates/rattler_repodata_gateway/src/fetch/mod.rs
@@ -1132,7 +1132,7 @@ mod test {
         // Create a directory with some repodata.
         let subdir_path = TempDir::new().unwrap();
         std::fs::write(subdir_path.path().join("repodata.json"), FAKE_REPO_DATA).unwrap();
-        let server = SimpleChannelServer::new(subdir_path.path());
+        let server = SimpleChannelServer::new(subdir_path.path()).await;
 
         // Download the data from the channel with an empty cache.
         let cache_dir = TempDir::new().unwrap();
@@ -1162,7 +1162,7 @@ mod test {
         // Create a directory with some repodata.
         let subdir_path = TempDir::new().unwrap();
         std::fs::write(subdir_path.path().join("repodata.json"), FAKE_REPO_DATA).unwrap();
-        let server = SimpleChannelServer::new(subdir_path.path());
+        let server = SimpleChannelServer::new(subdir_path.path()).await;
 
         // Download the data from the channel with an empty cache.
         let cache_dir = TempDir::new().unwrap();
@@ -1228,7 +1228,7 @@ mod test {
         .await
         .unwrap();
 
-        let server = SimpleChannelServer::new(subdir_path.path());
+        let server = SimpleChannelServer::new(subdir_path.path()).await;
 
         // Download the data from the channel with an empty cache.
         let cache_dir = TempDir::new().unwrap();
@@ -1270,7 +1270,7 @@ mod test {
         .await
         .unwrap();
 
-        let server = SimpleChannelServer::new(subdir_path.path());
+        let server = SimpleChannelServer::new(subdir_path.path()).await;
 
         // Download the data from the channel with an empty cache.
         let cache_dir = TempDir::new().unwrap();
@@ -1319,7 +1319,7 @@ mod test {
         .await
         .unwrap();
 
-        let server = SimpleChannelServer::new(subdir_path.path());
+        let server = SimpleChannelServer::new(subdir_path.path()).await;
 
         // Download the data from the channel with an empty cache.
         let cache_dir = TempDir::new().unwrap();
@@ -1366,7 +1366,7 @@ mod test {
         // The server is configured in such a way that if file `a` is requested but a file called
         // `a.gz` is available it will stream the `a.gz` file and report that its a `gzip` encoded
         // stream.
-        let server = SimpleChannelServer::new(subdir_path.path());
+        let server = SimpleChannelServer::new(subdir_path.path()).await;
 
         // Download the data from the channel
         let cache_dir = TempDir::new().unwrap();
@@ -1398,7 +1398,7 @@ mod test {
         // Create a directory with some repodata.
         let subdir_path = TempDir::new().unwrap();
         std::fs::write(subdir_path.path().join("repodata.json"), FAKE_REPO_DATA).unwrap();
-        let server = SimpleChannelServer::new(subdir_path.path());
+        let server = SimpleChannelServer::new(subdir_path.path()).await;
 
         let last_download_progress = Arc::new(AtomicU64::new(0));
         let last_download_progress_captured = last_download_progress.clone();
@@ -1450,7 +1450,7 @@ mod test {
         ));
 
         // Start a server to test the http error
-        let server = SimpleChannelServer::new(subdir_path.path());
+        let server = SimpleChannelServer::new(subdir_path.path()).await;
 
         // Download the "data" from the channel.
         let cache_dir = TempDir::new().unwrap();
