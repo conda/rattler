054beeb921fbeb8ab72fbf5838987d31a29ff3a3
Wackyator <pratapsinghtarun9@gmail.com>
2024-02-26 19:56:36 +0530

    docs: add doc test

--- py-rattler/rattler/package/about_json.py
+++ py-rattler/rattler/package/about_json.py
@@ -24,6 +24,15 @@ class AboutJson:
         from the file at the specified path, parse the JSON string and return the
         resulting object. If the file is not in a parsable format or if the file
         could not read, this function returns an error.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about
+        AboutJson()
+        >>>
+        ```
         """
         return AboutJson._from_py_about_json(PyAboutJson.from_path(Path(path)))
 
@@ -52,6 +61,18 @@ class AboutJson:
         For example, if the file is in JSON format, this function parses the JSON
         string and returns the resulting object. If the file is not in a parsable
         format, this function returns an error.
+
+        Examples
+        --------
+        ```python
+        >>> import json
+        >>> with open("../test-data/dummy-about.json", 'r') as file:
+        ...     json_str = json.dumps(json.load(file))
+        >>> about = AboutJson.from_str(json_str)
+        >>> about
+        AboutJson()
+        >>>
+        ```
         """
         return AboutJson._from_py_about_json(PyAboutJson.from_str(string))
 
@@ -62,6 +83,14 @@ class AboutJson:
 
         The path is relative to the root of the archive and includes any necessary
         directories.
+
+        Examples
+        --------
+        ```python
+        >>> AboutJson.package_path()
+        'info/about.json'
+        >>>
+        ```
         """
         return PyAboutJson.package_path()
 
@@ -69,6 +98,15 @@ class AboutJson:
     def channels(self) -> List[str]:
         """
         A list of channels that where used during the build.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about.channels
+        ['https://conda.anaconda.org/conda-forge']
+        >>>
+        ```
         """
         return self._inner.channels
 
@@ -76,13 +114,34 @@ class AboutJson:
     def description(self) -> Optional[str]:
         """
         Description of the package.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about.description
+        'A dummy description.'
+        >>>
+        ```
         """
-        return self._inner.description
+        if description := self._inner.description:
+            return description
+
+        return None
 
     @property
     def dev_url(self) -> List[str]:
         """
         A list of URLs to the development page of the package.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about.dev_url
+        ['https://github.com/mamba-org/rattler']
+        >>>
+        ```
         """
         return self._inner.dev_url
 
@@ -90,6 +149,15 @@ class AboutJson:
     def doc_url(self) -> List[str]:
         """
         A list of URLs to the documentation of the package.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about.doc_url
+        ['https://mamba-org.github.io/rattler/py-rattler/']
+        >>>
+        ```
         """
         return self._inner.doc_url
 
@@ -97,6 +165,15 @@ class AboutJson:
     def home(self) -> List[str]:
         """
         A list URL to the homepage of the package.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about.home
+        ['http://github.com/mamba-org/rattler']
+        >>>
+        ```
         """
         return self._inner.home
 
@@ -104,29 +181,78 @@ class AboutJson:
     def license(self) -> Optional[str]:
         """
         The license of the package.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about.license
+        'BSD-3-Clause'
+        >>>
+        ```
         """
-        return self._inner.license
+        if license := self._inner.license:
+            return license
+
+        return None
 
     @property
     def license_family(self) -> Optional[str]:
         """
         The license family of the package.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about.license_family
+        >>> type(about.license_family)
+        <class 'NoneType'>
+        >>>
+        ```
         """
-        return self._inner.license_family
+        if license_family := self._inner.license_family:
+            return license_family
+
+        return None
 
     @property
     def source_url(self) -> Optional[str]:
         """
         The URL to the latest source code of the package.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about.source_url
+        'https://github.com/mamba-org/rattler'
+        >>>
+        ```
         """
-        return self._inner.source_url
+        if source_url := self._inner.source_url:
+            return source_url
+
+        return None
 
     @property
     def summary(self) -> Optional[str]:
         """
         A Short summary description.
+
+        Examples
+        --------
+        ```python
+        >>> about = AboutJson.from_path("../test-data/dummy-about.json")
+        >>> about.summary
+        'A dummy summary.'
+        >>>
+        ```
         """
-        return self._inner.summary
+        if summary := self._inner.summary:
+            return summary
+
+        return None
 
     @classmethod
     def _from_py_about_json(cls, py_about_json: PyAboutJson) -> AboutJson:
