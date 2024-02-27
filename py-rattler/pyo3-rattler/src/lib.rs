9191b13cea53e301e6195a21561d43ba1c5379a1
Wackyator <pratapsinghtarun9@gmail.com>
2024-02-27 18:33:24 +0530

    feat: seperate bindings and lib into seperate crate

---
+++ py-rattler/pyo3-rattler/src/lib.rs
@@ -0,0 +1,58 @@
+mod about_json;
+mod channel;
+mod error;
+mod generic_virtual_package;
+mod index;
+mod linker;
+mod lock;
+mod match_spec;
+mod meta;
+mod nameless_match_spec;
+mod networking;
+mod package_name;
+mod platform;
+mod prefix_paths;
+mod record;
+mod repo_data;
+mod shell;
+mod solver;
+mod version;
+mod virtual_package;
+
+pub use about_json::PyAboutJson;
+pub use channel::{PyChannel, PyChannelConfig};
+pub use error::{
+    ActivationException, CacheDirException, ConvertSubdirException, DetectVirtualPackageException,
+    EnvironmentCreationException, FetchRepoDataException, InvalidChannelException,
+    InvalidMatchSpecException, InvalidPackageNameException, InvalidUrlException,
+    InvalidVersionException, IoException, LinkException, ParseArchException,
+    ParsePlatformException, PyRattlerError, SolverException, TransactionException,
+    VersionBumpException,
+};
+pub use generic_virtual_package::PyGenericVirtualPackage;
+pub use lock::{
+    PyEnvironment, PyLockChannel, PyLockFile, PyLockedPackage, PyPackageHashes, PyPypiPackageData,
+    PyPypiPackageEnvironmentData,
+};
+pub use match_spec::PyMatchSpec;
+pub use nameless_match_spec::PyNamelessMatchSpec;
+pub use networking::{authenticated_client::PyAuthenticatedClient, py_fetch_repo_data};
+pub use package_name::PyPackageName;
+pub use prefix_paths::PyPrefixPaths;
+pub use repo_data::{patch_instructions::PyPatchInstructions, sparse::PySparseRepoData, PyRepoData};
+pub use version::PyVersion;
+
+pub use pyo3::prelude::*;
+
+pub use index::py_index;
+pub use linker::py_link;
+pub use meta::get_rattler_version;
+pub use platform::{PyArch, PyPlatform};
+pub use record::PyRecord;
+pub use shell::{PyActivationResult, PyActivationVariables, PyActivator, PyShellEnum};
+pub use solver::py_solve;
+pub use virtual_package::PyVirtualPackage;
+
+pub fn pyo3_rattler_dummy() -> &'static str {
+    "lol"
+}
