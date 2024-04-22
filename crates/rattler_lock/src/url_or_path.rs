3e3239d3dcc06a6f6ed17e91820c4167e724dc9b
Bas Zalmstra <bas@prefix.dev>
2024-04-22 10:16:39 +0200

    fix: parse absolute paths on Windows correctly in lockfiles (#616)

--- crates/rattler_lock/src/url_or_path.rs
+++ crates/rattler_lock/src/url_or_path.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use serde_with::{DeserializeFromStr, SerializeDisplay};
 use std::path::Path;
 use std::{
@@ -74,12 +75,24 @@ pub enum PathOrUrlError {
     InvalidUrl(url::ParseError),
 }
 
+fn scheme_is_drive_letter(scheme: &str) -> bool {
+    let Some((drive_letter,)) = scheme.chars().collect_tuple() else {
+        return false;
+    };
+    drive_letter.is_ascii_alphabetic()
+}
+
 impl FromStr for UrlOrPath {
     type Err = PathOrUrlError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // First try to parse the string as a path.
         match Url::from_str(s) {
-            Ok(url) => Ok(UrlOrPath::Url(url)),
+            Ok(url) => Ok(if scheme_is_drive_letter(url.scheme()) {
+                UrlOrPath::Path(PathBuf::from(s))
+            } else {
+                UrlOrPath::Url(url)
+            }),
             Err(url::ParseError::RelativeUrlWithoutBase) => Ok(UrlOrPath::Path(PathBuf::from(s))),
             Err(e) => Err(PathOrUrlError::InvalidUrl(e)),
         }
@@ -93,21 +106,41 @@ mod test {
 
     #[test]
     fn test_from_str() {
-        assert_eq!(
-            UrlOrPath::from_str("https://example.com").unwrap(),
-            UrlOrPath::Url("https://example.com".parse().unwrap())
-        );
-        assert_eq!(
-            UrlOrPath::from_str("file:///path/to/file").unwrap(),
-            UrlOrPath::Url("file:///path/to/file".parse().unwrap())
-        );
-        assert_eq!(
-            UrlOrPath::from_str("/path/to/file").unwrap(),
-            UrlOrPath::Path("/path/to/file".parse().unwrap()),
-        );
-        assert_eq!(
-            UrlOrPath::from_str("./path/to/file").unwrap(),
-            UrlOrPath::Path("./path/to/file".parse().unwrap())
-        );
+        let urls = [
+            "https://conda.anaconda.org/conda-forge/linux-64/_libgcc_mutex-0.1-conda_forge.tar.bz2",
+        ];
+
+        for url in &urls {
+            assert_eq!(
+                UrlOrPath::from_str(url).unwrap(),
+                UrlOrPath::Url(url.parse().unwrap())
+            );
+        }
+
+        // Test paths
+        let paths = [
+            // Unix absolute paths
+            "/home/bob/test-file.txt",
+            // Windows absolute paths
+            "c:\\temp\\test-file.txt",
+            "c:/temp/test-file.txt",
+            // Relative paths
+            "./test-file.txt",
+            "../test-file.txt",
+            // UNC paths
+            "\\\\127.0.0.1\\c$\\temp\\test-file.txt",
+            "\\\\LOCALHOST\\c$\\temp\\test-file.txt",
+            "\\\\.\\c:\\temp\\test-file.txt",
+            "\\\\?\\c:\\temp\\test-file.txt",
+            "\\\\.\\UNC\\LOCALHOST\\c$\\temp\\test-file.txt",
+            "\\\\127.0.0.1\\c$\\temp\\test-file.txt",
+        ];
+
+        for path in &paths {
+            assert_eq!(
+                UrlOrPath::from_str(path).unwrap(),
+                UrlOrPath::Path(path.parse().unwrap())
+            );
+        }
     }
 }
