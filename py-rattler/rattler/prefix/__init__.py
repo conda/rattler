5b27f49901321deca787e96b00308860be26644f
Alfredo Luque <alfredo.luque@airbnb.com>
2024-04-24 02:25:29 -0700

    feat: Expose paths_data as PathEntry in py-rattler (#620)

--- py-rattler/rattler/prefix/__init__.py
+++ py-rattler/rattler/prefix/__init__.py
@@ -1,4 +1,4 @@
 from rattler.prefix.prefix_record import PrefixRecord
-from rattler.prefix.prefix_paths import PrefixPaths
+from rattler.prefix.prefix_paths import PrefixPaths, PrefixPathsEntry, PrefixPathType
 
-__all__ = ["PrefixRecord", "PrefixPaths"]
+__all__ = ["PrefixRecord", "PrefixPaths", "PrefixPathsEntry", "PrefixPathType"]
