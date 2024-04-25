fe197d379ca69d2d3dbb20f1496fcb6e3e367bb9
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-04-25 15:32:28 +0200

    use gcs only conditionally

--- crates/rattler_networking/src/lib.rs
+++ crates/rattler_networking/src/lib.rs
@@ -3,13 +3,17 @@
 //! Networking utilities for Rattler, specifically authenticating requests
 pub use authentication_middleware::AuthenticationMiddleware;
 pub use authentication_storage::{authentication::Authentication, storage::AuthenticationStorage};
-pub use gcs_middleware::GCSMiddleware;
 pub use mirror_middleware::MirrorMiddleware;
 pub use oci_middleware::OciMiddleware;
 
+#[cfg(feature = "gcs")]
+pub mod gcs_middleware;
+#[cfg(feature = "gcs")]
+pub use gcs_middleware::GCSMiddleware;
+
 pub mod authentication_middleware;
 pub mod authentication_storage;
-pub mod gcs_middleware;
+
 pub mod mirror_middleware;
 pub mod oci_middleware;
 pub mod retry_policies;
