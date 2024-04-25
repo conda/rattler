a4e3bd21c27b069d08f3904a37c3a95f36a820f7
Clement Chane <151473645+clement-chaneching@users.noreply.github.com>
2024-04-25 23:41:07 +1000

    feat: Add GCS support for rattler auth (#605)

--- crates/rattler_networking/src/lib.rs
+++ crates/rattler_networking/src/lib.rs
@@ -6,8 +6,14 @@ pub use authentication_storage::{authentication::Authentication, storage::Authen
 pub use mirror_middleware::MirrorMiddleware;
 pub use oci_middleware::OciMiddleware;
 
+#[cfg(feature = "gcs")]
+pub mod gcs_middleware;
+#[cfg(feature = "gcs")]
+pub use gcs_middleware::GCSMiddleware;
+
 pub mod authentication_middleware;
 pub mod authentication_storage;
+
 pub mod mirror_middleware;
 pub mod oci_middleware;
 pub mod retry_policies;
