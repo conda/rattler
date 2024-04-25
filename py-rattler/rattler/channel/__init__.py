926a876fcfe1774adfdd094637ef467d78594744
Benjamin Lowry <benjaminpaullowry@gmail.com>
2024-04-25 09:40:49 -0400

    feat: add channel priority to solve task and expose to python solve (#598)

--- py-rattler/rattler/channel/__init__.py
+++ py-rattler/rattler/channel/__init__.py
@@ -1,4 +1,5 @@
 from rattler.channel.channel import Channel
 from rattler.channel.channel_config import ChannelConfig
+from rattler.channel.channel_priority import ChannelPriority
 
-__all__ = ["Channel", "ChannelConfig"]
+__all__ = ["Channel", "ChannelConfig", "ChannelPriority"]
