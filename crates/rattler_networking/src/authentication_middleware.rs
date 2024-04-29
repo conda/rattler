0710445e1ef90e118562e876a07b91b2eb495760
Bela Stoyan <bela.stoyan@gmail.com>
2024-04-29 22:53:25 +0200

    add unit test

--- crates/rattler_networking/src/authentication_middleware.rs
+++ crates/rattler_networking/src/authentication_middleware.rs
@@ -399,4 +399,31 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn test_rattler_auth_file_env_var_handling() -> anyhow::Result<()> {
+        let tdir = tempdir()?;
+
+        std::env::set_var(
+            "RATTLER_AUTH_FILE",
+            tdir.path()
+                .to_path_buf()
+                .join("auth.json")
+                .to_str().unwrap(),
+        );
+        let storage = AuthenticationStorage::default();
+        std::env::remove_var("RATTLER_AUTH_FILE");
+
+        let host = "test.example.com";
+        let authentication = Authentication::CondaToken("testtoken".to_string());
+        storage.store(host, &authentication)?;
+
+        let file = tdir.path().join("auth.json");
+        assert_eq!(
+            std::fs::read_to_string(file)?,
+            "{\"test.example.com\":{\"CondaToken\":\"testtoken\"}}"
+        );
+
+        Ok(())
+    }
 }
