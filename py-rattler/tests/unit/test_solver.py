ab2bd4db2a2c8f0319290d5a78d1d200ea1e7e46
Alfredo Luque <alfredo.luque@airbnb.com>
2024-05-10 13:10:13 -0700

    fix: Address (#648) Missing FileMode causes an AttributeError in py-rattler and fix missing properties (#651)

--- py-rattler/tests/unit/test_solver.py
+++ py-rattler/tests/unit/test_solver.py
@@ -3,8 +3,11 @@ import pytest
 from rattler import (
     solve,
     ChannelPriority,
-    RepoDataRecord, Channel, Gateway,
+    RepoDataRecord,
+    Channel,
+    Gateway,
 )
+
 
 @pytest.mark.asyncio
 async def test_solve(gateway: Gateway, conda_forge_channel: Channel) -> None:
@@ -21,7 +24,9 @@ async def test_solve(gateway: Gateway, conda_forge_channel: Channel) -> None:
 
 
 @pytest.mark.asyncio
-async def test_solve_channel_priority_disabled(gateway: Gateway, pytorch_channel: Channel, conda_forge_channel: Channel) -> None:
+async def test_solve_channel_priority_disabled(
+    gateway: Gateway, pytorch_channel: Channel, conda_forge_channel: Channel
+) -> None:
     solved_data = await solve(
         [conda_forge_channel, pytorch_channel],
         ["linux-64"],
@@ -32,5 +37,8 @@ async def test_solve_channel_priority_disabled(gateway: Gateway, pytorch_channel
 
     assert isinstance(solved_data, list)
     assert isinstance(solved_data[0], RepoDataRecord)
-    assert list(filter(lambda r: r.file_name.startswith("pytorch-cpu-0.4.1-py36_cpu_1"), solved_data))[0].channel == pytorch_channel.base_url
+    assert (
+        list(filter(lambda r: r.file_name.startswith("pytorch-cpu-0.4.1-py36_cpu_1"), solved_data))[0].channel
+        == pytorch_channel.base_url
+    )
     assert len(solved_data) == 32
