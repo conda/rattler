7417851cf241fe8758e91226864983d55763a7db
nichmor <nmorkotilo@gmail.com>
2024-05-10 11:25:55 +0300

    misc: update test

--- crates/rattler_package_streaming/tests/write.rs
+++ crates/rattler_package_streaming/tests/write.rs
@@ -122,6 +122,10 @@ fn compare_two_conda_archives(p1: &Path, p2: &Path) {
     println!("Comparing {p1:?} and {p2:?}");
     let mut archive1 = File::open(p1).unwrap();
     let mut archive2 = File::open(p2).unwrap();
+    
+    let archive1_metadata = archive1.metadata().unwrap();
+    let archive2_metadata = archive1.metadata().unwrap();
+
     // open outer zip file
     let mut zip1 = zip::ZipArchive::new(&mut archive1).unwrap();
     let mut zip2 = zip::ZipArchive::new(&mut archive2).unwrap();
@@ -135,6 +139,14 @@ fn compare_two_conda_archives(p1: &Path, p2: &Path) {
     let _mstr1 = std::str::from_utf8(&metadata_bytes1).unwrap();
     let metadata_bytes2 = metadata2.bytes().collect::<Result<Vec<u8>, _>>().unwrap();
     let _mstr2 = std::str::from_utf8(&metadata_bytes2).unwrap();
+
+    // compare last modified time
+
+    let last_mtime1 = archive1_metadata.modified().unwrap();
+    let last_mtime2 = archive2_metadata.modified().unwrap();
+
+    assert_eq!(last_mtime1, last_mtime2);
+
 
     // compare metadata.json files
 
