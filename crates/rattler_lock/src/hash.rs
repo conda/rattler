65d394f69c2e50c6b08441afd98167a9394cb912
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-07-08 09:10:39 +0200

    Revert "feat: Only save md5 in lock file if no sha256 is present (#764)"
    This reverts commit 4ee9217c027ac7c5093fc7c4e715cacb8fc259ca.

--- crates/rattler_lock/src/hash.rs
+++ crates/rattler_lock/src/hash.rs
@@ -7,8 +7,9 @@ use serde_with::skip_serializing_none;
 /// This implementation of the `Deserialize` trait for the `PackageHashes` struct
 ///
 /// It expects the input to have either a `md5` field, a `sha256` field, or both.
-/// If the `sha256` field is present, it constructs a `Sha256` instance with its value.
+/// If both fields are present, it constructs a `Md5Sha256` instance with their values.
 /// If only the `md5` field is present, it constructs a `Md5` instance with its value.
+/// If only the `sha256` field is present, it constructs a `Sha256` instance with its value.
 /// If neither field is present it returns an error
 #[derive(Eq, PartialEq, Hash, Clone, Debug)]
 pub enum PackageHashes {
@@ -16,6 +17,8 @@ pub enum PackageHashes {
     Md5(Md5Hash),
     /// Contains as Sha256 Hash
     Sha256(Sha256Hash),
+    /// Contains both hashes
+    Md5Sha256(Md5Hash, Sha256Hash),
 }
 
 impl PartialOrd for PackageHashes {
@@ -33,10 +36,11 @@ impl Ord for PackageHashes {
 impl PackageHashes {
     /// Create correct enum from hashes
     pub fn from_hashes(md5: Option<Md5Hash>, sha256: Option<Sha256Hash>) -> Option<PackageHashes> {
-        use PackageHashes::{Md5, Sha256};
+        use PackageHashes::{Md5, Md5Sha256, Sha256};
         match (md5, sha256) {
-            (_, Some(sha256)) => Some(Sha256(sha256)),
             (Some(md5), None) => Some(Md5(md5)),
+            (None, Some(sha256)) => Some(Sha256(sha256)),
+            (Some(md5), Some(sha256)) => Some(Md5Sha256(md5, sha256)),
             (None, None) => None,
         }
     }
@@ -45,7 +49,7 @@ impl PackageHashes {
     pub fn sha256(&self) -> Option<&Sha256Hash> {
         match self {
             PackageHashes::Md5(_) => None,
-            PackageHashes::Sha256(sha256) => Some(sha256),
+            PackageHashes::Sha256(sha256) | PackageHashes::Md5Sha256(_, sha256) => Some(sha256),
         }
     }
 
@@ -53,7 +57,7 @@ impl PackageHashes {
     pub fn md5(&self) -> Option<&Md5Hash> {
         match self {
             PackageHashes::Sha256(_) => None,
-            PackageHashes::Md5(md5) => Some(md5),
+            PackageHashes::Md5(md5) | PackageHashes::Md5Sha256(md5, _) => Some(md5),
         }
     }
 
@@ -62,6 +66,7 @@ impl PackageHashes {
         match self {
             PackageHashes::Sha256(sha256) => sha256.to_vec(),
             PackageHashes::Md5(md5) => md5.to_vec(),
+            PackageHashes::Md5Sha256(md5, sha256) => [md5.to_vec(), sha256.to_vec()].concat(),
         }
     }
 }
@@ -78,7 +83,7 @@ impl Serialize for PackageHashes {
     where
         S: Serializer,
     {
-        use PackageHashes::{Md5, Sha256};
+        use PackageHashes::{Md5, Md5Sha256, Sha256};
         let raw = match self {
             Md5(hash) => RawPackageHashes {
                 md5: Some(SerializableHash::from(*hash)),
@@ -88,6 +93,10 @@ impl Serialize for PackageHashes {
                 md5: None,
                 sha256: Some(SerializableHash::from(*hash)),
             },
+            Md5Sha256(md5hash, sha) => RawPackageHashes {
+                md5: Some(SerializableHash::from(*md5hash)),
+                sha256: Some(SerializableHash::from(*sha)),
+            },
         };
         raw.serialize(serializer)
     }
@@ -96,22 +105,24 @@ impl Serialize for PackageHashes {
 // This implementation of the `Deserialize` trait for the `PackageHashes` struct
 //
 // It expects the input to have either a `md5` field, a `sha256` field, or both.
-// If the `sha256` field is present, it constructs a `Sha256` instance with its value.
+// If both fields are present, it constructs a `Md5Sha256` instance with their values.
 // If only the `md5` field is present, it constructs a `Md5` instance with its value.
+// If only the `sha256` field is present, it constructs a `Sha256` instance with its value.
 // If neither field is present it returns an error
 impl<'de> Deserialize<'de> for PackageHashes {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
-        use PackageHashes::{Md5, Sha256};
+        use PackageHashes::{Md5, Md5Sha256, Sha256};
         let temp = RawPackageHashes::deserialize(deserializer)?;
         Ok(match (temp.md5, temp.sha256) {
-            (_, Some(sha)) => Sha256(sha.into()),
+            (Some(md5), Some(sha)) => Md5Sha256(md5.into(), sha.into()),
             (Some(md5), None) => Md5(md5.into()),
+            (None, Some(sha)) => Sha256(sha.into()),
             _ => {
                 return Err(D::Error::custom(
-                    "Expected `sha256` field, `md5` field or both",
+                    "Expected `sha256` field `md5` field or both",
                 ))
             }
         })
@@ -129,8 +140,9 @@ mod test {
           md5: 4eccaeba205f0aed9ac3a9ea58568ca3
           sha256: f240217476e148e825420c6bc3a0c0efb08c0718b7042fae960400c02af858a3
     "#;
+
         let result: PackageHashes = from_str(yaml).unwrap();
-        assert!(matches!(result, PackageHashes::Sha256(_)));
+        assert!(matches!(result, PackageHashes::Md5Sha256(_, _)));
 
         let yaml = r#"
           md5: 4eccaeba205f0aed9ac3a9ea58568ca3
