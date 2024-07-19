10c992094616cc794b3dbaad46241641d848c743
Bas Zalmstra <bas@prefix.dev>
2024-07-19 16:48:16 +0200

    test: add failing test

--- crates/rattler/src/install/clobber_registry.rs
+++ crates/rattler/src/install/clobber_registry.rs
@@ -10,8 +10,8 @@ use fs_err as fs;
 use indexmap::IndexSet;
 use itertools::Itertools;
 use rattler_conda_types::{
-    package::{IndexJson, PathsEntry},
     PackageName, PrefixRecord,
+    package::{IndexJson, PathsEntry},
 };
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -443,12 +443,12 @@ mod tests {
 
     use insta::assert_yaml_snapshot;
     use rand::seq::SliceRandom;
-    use rattler_conda_types::{prefix::Prefix, Platform, PrefixRecord, RepoDataRecord, Version};
+    use rattler_conda_types::{Platform, PrefixRecord, RepoDataRecord, Version, prefix::Prefix};
     use transaction::TransactionOperation;
 
     use crate::{
         get_repodata_record, get_test_data_dir,
-        install::{test_utils::*, transaction, InstallDriver, InstallOptions, PythonInfo},
+        install::{InstallDriver, InstallOptions, PythonInfo, test_utils::*, transaction},
         package_cache::PackageCache,
     };
 
@@ -1436,5 +1436,68 @@ mod tests {
         .await;
 
         assert!(target_prefix.path().join("xbin").is_file());
+    }
+
+    // This used to hit an expect in the clobbering code
+    #[tokio::test]
+    async fn test_directory_clobber() {
+        // Create a transaction
+        let repodata_record_with_dir = get_repodata_record(
+            get_test_data_dir().join("clobber/clobber-with-dir-0.1.0-h4616a5c_0.conda"),
+        );
+        let repodata_record_without_dir = get_repodata_record(
+            get_test_data_dir().join("clobber/clobber-without-dir-0.1.0-h4616a5c_0.conda"),
+        );
+
+        let transaction = transaction::Transaction::<PrefixRecord, RepoDataRecord> {
+            operations: vec![TransactionOperation::Install(repodata_record_with_dir)],
+            python_info: None,
+            current_python_info: None,
+            platform: Platform::current(),
+        };
+
+        // execute transaction
+        let target_prefix = tempfile::tempdir().unwrap();
+        let packages_dir = tempfile::tempdir().unwrap();
+        let cache = PackageCache::new(packages_dir.path());
+
+        execute_transaction(
+            transaction,
+            target_prefix.path(),
+            &reqwest_middleware::ClientWithMiddleware::from(reqwest::Client::new()),
+            &cache,
+            &InstallDriver::default(),
+            &InstallOptions::default(),
+        )
+        .await;
+
+        let prefix_records = PrefixRecord::collect_from_prefix(target_prefix.path()).unwrap();
+
+        // remove one of the clobbering files
+        let transaction = transaction::Transaction::<PrefixRecord, RepoDataRecord> {
+            operations: vec![
+                TransactionOperation::Remove(prefix_records[0].clone()),
+                TransactionOperation::Install(repodata_record_without_dir),
+            ],
+            python_info: None,
+            current_python_info: None,
+            platform: Platform::current(),
+        };
+
+        let install_driver = InstallDriver::builder()
+            .with_prefix_records(&prefix_records)
+            .finish();
+
+        execute_transaction(
+            transaction,
+            target_prefix.path(),
+            &reqwest_middleware::ClientWithMiddleware::from(reqwest::Client::new()),
+            &cache,
+            &install_driver,
+            &InstallOptions::default(),
+        )
+        .await;
+
+        assert_check_files(&target_prefix.path(), &["dir"]);
     }
 }
