061c70109de43b0a9f70301f92a8953e8b4fa9ce
jpcorreia99 <jcorreia@palantir.com>
2024-07-23 15:41:52 +0100

    Revert "read locally"
    This reverts commit f7b1632f94513f592636993a1160c10835914a2d.

--- crates/rattler_package_streaming/src/read.rs
+++ crates/rattler_package_streaming/src/read.rs
@@ -5,10 +5,6 @@ use super::{ExtractError, ExtractResult};
 use std::mem::ManuallyDrop;
 use std::{ffi::OsStr, io::Read, path::Path};
 use zip::read::read_zipfile_from_stream;
-use std::fs::File;
-use std::io::{self, Write, Seek};
-use zip::read::ZipArchive;
-use rattler_digest::{HashingReader, Sha256, Md5};
 
 /// Returns the `.tar.bz2` as a decompressed `tar::Archive`. The `tar::Archive` can be used to
 /// extract the files from it, or perform introspection.
@@ -48,50 +44,42 @@ pub fn extract_tar_bz2(
 }
 
 /// Extracts the contents of a `.conda` package archive.
-/// Extracts the contents of a `.conda` package archive.
 pub fn extract_conda(reader: impl Read, destination: &Path) -> Result<ExtractResult, ExtractError> {
-    // Construct the destination path if it doesn't exist yet
+    // Construct the destination path if it doesnt exist yet
     std::fs::create_dir_all(destination).map_err(ExtractError::CouldNotCreateDestination)?;
 
-    // Create a temporary file to store the downloaded zip file
-    let mut temp_file = tempfile::tempfile().map_err(|e| ExtractError::IoError(e))?;
-    std::io::copy(&mut reader.take(u64::MAX), &mut temp_file).map_err(|e| ExtractError::IoError(e))?;
-
-    // Rewind the file to the beginning
-    temp_file.seek(io::SeekFrom::Start(0)).map_err(|e| ExtractError::IoError(e))?;
-    
-
-    // Open the zip archive\
-    let mut zip = ZipArchive::new(&mut temp_file).map_err(|e| ExtractError::ZipError(e))?;
+    // Wrap the reading in aditional readers that will compute the hashes of the file while its
+    // being read.
+    let sha256_reader = rattler_digest::HashingReader::<_, rattler_digest::Sha256>::new(reader);
+    let mut md5_reader =
+        rattler_digest::HashingReader::<_, rattler_digest::Md5>::new(sha256_reader);
 
     // Iterate over all entries in the zip-file and extract them one-by-one
-    for i in 0..zip.len() {
-        let mut file = zip.by_index(i).map_err(|e| ExtractError::ZipError(e))?;
-        let outpath = destination.join(file.sanitized_name());
+    while let Some(file) = read_zipfile_from_stream(&mut md5_reader)? {
+        // If an error occurs while we are reading the contents of the zip we don't want to
+        // seek to the end of the file. Using [`ManuallyDrop`] we prevent `drop` to be called on
+        // the `file` in case the stack unwinds.
+        let mut file = ManuallyDrop::new(file);
 
-        if file.name().ends_with(".tar.zst") {
-            let mut tar_zst_file = File::create(&outpath).map_err(|e| ExtractError::IoError(e))?;
-            std::io::copy(&mut file, &mut tar_zst_file).map_err(|e| ExtractError::IoError(e))?;
-            tar_zst_file.seek(io::SeekFrom::Start(0)).map_err(|e| ExtractError::IoError(e))?;
-            stream_tar_zst(tar_zst_file)?.unpack(destination)?;
+        if file
+            .mangled_name()
+            .file_name()
+            .map(OsStr::to_string_lossy)
+            .map_or(false, |file_name| file_name.ends_with(".tar.zst"))
+        {
+            stream_tar_zst(&mut *file)?.unpack(destination)?;
         } else {
-            if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    std::fs::create_dir_all(&p).map_err(|e| ExtractError::IoError(e))?;
-                }
-            }
-            let mut outfile = File::create(&outpath).map_err(|e| ExtractError::IoError(e))?;
-            std::io::copy(&mut file, &mut outfile).map_err(|e| ExtractError::IoError(e))?;
+            // Manually read to the end of the stream if that didn't happen.
+            std::io::copy(&mut *file, &mut std::io::sink())?;
         }
+
+        // Take the file out of the [`ManuallyDrop`] to properly drop it.
+        let _ = ManuallyDrop::into_inner(file);
     }
 
     // Read the file to the end to make sure the hash is properly computed.
-    std::io::copy(&mut temp_file, &mut std::io::sink()).map_err(|e| ExtractError::IoError(e))?;
+    std::io::copy(&mut md5_reader, &mut std::io::sink())?;
 
-    let sha256_reader = rattler_digest::HashingReader::<_, rattler_digest::Sha256>::new("abc");
-  
-    let md5_reader =
-    rattler_digest::HashingReader::<_, rattler_digest::Md5>::new(sha256_reader);
     // Get the hashes
     let (sha256_reader, md5) = md5_reader.finalize();
     let (_, sha256) = sha256_reader.finalize();
