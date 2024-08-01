b294c975dec3c9ca03179e669af85fe1712696dc
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-08-01 16:15:33 +0200

    fix doctest

--- crates/rattler_redaction/src/lib.rs
+++ crates/rattler_redaction/src/lib.rs
@@ -14,11 +14,11 @@ pub const DEFAULT_REDACTION_STR: &str = "********";
 /// # Example
 ///
 /// ```rust
-/// # use rattler_redaction::{redact_known_secrets_from_url, DEFAULT_REDACTION_STR};
+/// # use rattler_redaction::{redact_known_secrets_from_url, Redact, DEFAULT_REDACTION_STR};
 /// # use url::Url;
 ///
 /// let url = Url::parse("https://conda.anaconda.org/t/12345677/conda-forge/noarch/repodata.json").unwrap();
-/// let redacted_url = redact_known_secrets_from_url(&url, DEFAULT_REDACTION_STR).unwrap_or(url);
+/// let redacted_url = redact_known_secrets_from_url(&url, DEFAULT_REDACTION_STR).unwrap_or(url.clone());
 /// // or you can use the shorthand
 /// let redacted_url = url.redact();
 /// ```
