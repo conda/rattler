01fb7a235105449f46dc59cbeb1ac0746c2536b5
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-20 11:38:55 -0700

    add return annotation to tests

--- py-rattler/tests/unit/test_matchspec.py
+++ py-rattler/tests/unit/test_matchspec.py
@@ -1,13 +1,13 @@
 from rattler import MatchSpec
 
 
-def test_parse_channel_from_canonical_name():
+def test_parse_channel_from_canonical_name() -> None:
     m = MatchSpec("conda-forge::python[version=3.9]")
     assert m.channel.name == "conda-forge"
     assert m.channel.base_url == "https://conda.anaconda.org/conda-forge/"
 
 
-def test_parse_channel_from_url():
+def test_parse_channel_from_url() -> None:
     m = MatchSpec("https://conda.anaconda.org/conda-forge::python[version=3.9]")
     assert m.channel.name == "conda-forge"
     assert m.channel.base_url == "https://conda.anaconda.org/conda-forge/"
