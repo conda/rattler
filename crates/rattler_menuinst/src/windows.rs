c4b172f964b1f1cd058e0b943b3a8e01b104aa4e
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-09-29 22:41:35 +0200

    improve the state of things on Windows

--- crates/rattler_menuinst/src/windows.rs
+++ crates/rattler_menuinst/src/windows.rs
@@ -1,12 +1,15 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
+use crate::{schema, MenuInstError, MenuMode};
+use fs_err as fs;
 mod knownfolders;
 mod registry;
+mod create_shortcut;
 
 struct Directories {
     start_menu_location: PathBuf,
-    quick_launch_location: PathBuf,
-    desktop_location: PathBuf,
+    quick_launch_location: Option<PathBuf>,
+    desktop_location: Option<PathBuf>,
 }
 
 impl Directories {
@@ -15,11 +18,9 @@ impl Directories {
             known_folders::get_known_folder_path(known_folders::KnownFolder::StartMenu)
                 .expect("Failed to get start menu location");
         let quick_launch_location =
-            known_folders::get_known_folder_path(known_folders::KnownFolder::QuickLaunch)
-                .expect("Failed to get quick launch location");
+            known_folders::get_known_folder_path(known_folders::KnownFolder::QuickLaunch);
         let desktop_location =
-            known_folders::get_known_folder_path(known_folders::KnownFolder::Desktop)
-                .expect("Failed to get desktop location");
+            known_folders::get_known_folder_path(known_folders::KnownFolder::Desktop);
 
         Directories {
             start_menu_location,
@@ -30,8 +31,54 @@ impl Directories {
 }
 
 pub struct WindowsMenu {
-    name: String,
-    mode: String,
+    directories: Directories,
+    item: schema::Windows,
+    mode: MenuMode,
     prefix: PathBuf,
-    base_prefix: PathBuf,
 }
+
+impl WindowsMenu {
+    pub fn new(item: schema::Windows, prefix: &Path, mode: MenuMode) -> WindowsMenu {
+        let directories = Directories::create();
+
+        WindowsMenu {
+            directories,
+            item,
+            mode,
+            prefix: prefix.to_path_buf(),
+        }
+    }
+
+    pub fn create(&self) -> Result<PathBuf, MenuInstError> {
+        tracing::debug!("Creating {:?}", self.directories.start_menu_location);
+        fs::create_dir_all(&self.directories.start_menu_location)?;
+
+        if let Some(ref quick_launch_location) = self.directories.quick_launch_location {
+            fs::create_dir_all(quick_launch_location)?;
+        }
+
+        if let Some(ref desktop_location) = self.directories.desktop_location {
+            fs::create_dir_all(desktop_location)?;
+        }
+
+        Ok(self.directories.start_menu_location.clone())
+    }
+
+    pub fn remove(&self) -> Result<PathBuf, MenuInstError> {
+        let menu_location = &self.directories.start_menu_location;
+        if menu_location.exists() {
+            if menu_location.read_dir()?.next().is_none() {
+                tracing::info!("Removing {menu_location:?}", );
+                fs::remove_dir_all(menu_location)?;
+            }
+        }
+        Ok(self.directories.start_menu_location.clone())
+    }
+}
+
+struct WindowsMenuItem {
+    item: schema::Windows,
+    prefix: PathBuf,
+}
+
+
