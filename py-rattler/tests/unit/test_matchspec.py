4cea16ef76dda9b1f7b054126034af2367153679
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-10-01 15:36:33 -0700

    add no channel test

--- py-rattler/tests/unit/test_matchspec.py
+++ py-rattler/tests/unit/test_matchspec.py
@@ -23,7 +23,14 @@ def test_parse_channel_from_url_filesystem() -> None:
 
 
 def test_parse_channel_from_url_localhost() -> None:
-    m = MatchSpec("http://localhost:8000/get/channel0::python[version=3.9]")
+    m = MatchSpec("http://localhost:8000/channel0::python[version=3.9]")
     assert m.channel is not None
     assert m.channel.name == "channel0"
     assert m.channel.base_url == "http://localhost:8000/get/channel0"
+
+
+def test_parse_no_channel() -> None:
+    m = MatchSpec("python[version=3.9]")
+    assert m.channel is None
+    assert m.name.normalized == "python"
+    assert m.version == "==3.9"
