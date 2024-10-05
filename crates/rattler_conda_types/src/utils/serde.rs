930065d4095a3017e74ac64835889561432e2279
Tim de Jager <tim@prefix.dev>
2024-10-05 14:06:12 +0200

    feat: removed seemingly unused code

--- crates/rattler_conda_types/src/utils/serde.rs
+++ crates/rattler_conda_types/src/utils/serde.rs
@@ -139,62 +139,6 @@ impl SerializeAs<chrono::DateTime<chrono::Utc>> for Timestamp {
     }
 }
 
-/// Used with `serde_with` to serialize a collection as a sorted collection.
-// #[derive(Default)]
-// pub(crate) struct Ordered<T>(PhantomData<T>);
-
-// impl<'de, T: Eq + Hash, S: BuildHasher + Default, TAs> DeserializeAs<'de, HashSet<T, S>>
-//     for Ordered<TAs>
-// where
-//     TAs: DeserializeAs<'de, T>,
-// {
-//     fn deserialize_as<D>(deserializer: D) -> Result<HashSet<T, S>, D::Error>
-//     where
-//         D: Deserializer<'de>,
-//     {
-//         let content =
-//             DeserializeAsWrap::<Vec<T>, Vec<TAs>>::deserialize(deserializer)?.into_inner();
-//         Ok(content.into_iter().collect())
-//     }
-// }
-
-// impl<T: Ord, HS, TAs: SerializeAs<T>> SerializeAs<HashSet<T, HS>> for Ordered<TAs> {
-//     fn serialize_as<S>(source: &HashSet<T, HS>, serializer: S) -> Result<S::Ok, S::Error>
-//     where
-//         S: Serializer,
-//     {
-//         let mut elements = source.iter().collect_vec();
-//         elements.sort();
-//         SerializeAsWrap::<Vec<&T>, Vec<&TAs>>::new(&elements).serialize(serializer)
-//     }
-// }
-
-// impl<'de, T: Ord, TAs> DeserializeAs<'de, Vec<T>> for Ordered<TAs>
-// where
-//     TAs: DeserializeAs<'de, T>,
-// {
-//     fn deserialize_as<D>(deserializer: D) -> Result<Vec<T>, D::Error>
-//     where
-//         D: Deserializer<'de>,
-//     {
-//         let mut content =
-//             DeserializeAsWrap::<Vec<T>, Vec<TAs>>::deserialize(deserializer)?.into_inner();
-//         content.sort();
-//         Ok(content)
-//     }
-// }
-
-// impl<T: Ord, TAs: SerializeAs<T>> SerializeAs<Vec<T>> for Ordered<TAs> {
-//     fn serialize_as<S>(source: &Vec<T>, serializer: S) -> Result<S::Ok, S::Error>
-//     where
-//         S: Serializer,
-//     {
-//         let mut elements = source.iter().collect_vec();
-//         elements.sort();
-//         SerializeAsWrap::<Vec<&T>, Vec<&TAs>>::new(&elements).serialize(serializer)
-//     }
-// }
-
 /// A helper struct to deserialize types from a string without checking the string.
 pub struct DeserializeFromStrUnchecked;
 
