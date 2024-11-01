e8c631c2118c3c8be9df05b13b8ba107d9e552a2
Pavel Zwerschke <pavelzw@gmail.com>
2024-11-01 13:20:27 +0100

    proper error handling

--- py-rattler/src/networking/middleware.rs
+++ py-rattler/src/networking/middleware.rs
@@ -1,9 +1,11 @@
-use pyo3::{pyclass, pymethods, FromPyObject};
+use pyo3::{pyclass, pymethods, FromPyObject, PyResult};
 use rattler_networking::{
     mirror_middleware::Mirror, AuthenticationMiddleware, AuthenticationStorage, MirrorMiddleware,
 };
 use std::collections::HashMap;
 use url::Url;
+
+use crate::error::PyRattlerError;
 
 #[derive(FromPyObject)]
 pub enum PyMiddleware {
@@ -21,26 +23,28 @@ pub struct PyMirrorMiddleware {
 #[pymethods]
 impl PyMirrorMiddleware {
     #[new]
-    pub fn __init__(inner: HashMap<String, Vec<String>>) -> Self {
-        let map = inner
-            .into_iter()
-            .map(|(k, v)| {
-                (
-                    Url::parse(&k).unwrap(),
-                    v.into_iter()
+    pub fn __init__(inner: HashMap<String, Vec<String>>) -> PyResult<Self> {
+        let mut map = HashMap::new();
+        for (k, v) in inner {
+            let key = Url::parse(&k).map_err(PyRattlerError::from)?;
+            let value = v
+                .into_iter()
+                .map(|url| {
+                    Url::parse(&url)
                         .map(|url| Mirror {
-                            url: Url::parse(&url).unwrap(),
+                            url,
                             no_zstd: false,
                             no_bz2: false,
                             no_jlap: false,
                             max_failures: None,
                         })
-                        .collect(),
-                )
-            })
-            .collect();
+                        .map_err(PyRattlerError::from)
+                })
+                .collect::<Result<Vec<Mirror>, PyRattlerError>>()?;
+            map.insert(key, value);
+        }
 
-        Self { inner: map }
+        Ok(Self { inner: map })
     }
 }
 
