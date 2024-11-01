addb3c4ccd76651ee8a15a4f33cfcf8a4352ce4d
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-11-01 13:33:24 +0100

    update google-cloud-auth and rename feature to gcs

--- crates/rattler_networking/src/gcs_middleware.rs
+++ crates/rattler_networking/src/gcs_middleware.rs
@@ -1,6 +1,7 @@
 //! Middleware to handle `gcs://` URLs to pull artifacts from an GCS
 use async_trait::async_trait;
-use google_cloud_auth::project::{create_token_source, Config};
+use google_cloud_auth::{project::Config, token::DefaultTokenSourceProvider};
+use google_cloud_token::TokenSourceProvider;
 use reqwest::{Request, Response};
 use reqwest_middleware::{Middleware, Next, Result as MiddlewareResult};
 use url::Url;
@@ -40,23 +41,24 @@ async fn authenticate_with_google_cloud(mut req: Request) -> MiddlewareResult<Re
         "https://www.googleapis.com/auth/cloud-platform",
         "https://www.googleapis.com/auth/devstorage.read_only",
     ];
-    match create_token_source(Config {
-        audience: Some(audience),
-        scopes: Some(&scopes),
-        sub: None,
-    })
-    .await
-    {
-        Ok(ts) => match ts.token().await {
+    let config = Config::default()
+        .with_audience(audience)
+        .with_scopes(&scopes);
+
+    match DefaultTokenSourceProvider::new(config).await {
+        Ok(provider) => match provider.token_source().token().await {
             Ok(token) => {
-                let bearer_auth = format!("Bearer {}", token.access_token);
+                let bearer_auth = format!("Bearer {}", token);
                 let header_value = reqwest::header::HeaderValue::from_str(&bearer_auth)
                     .map_err(reqwest_middleware::Error::middleware)?;
                 req.headers_mut()
                     .insert(reqwest::header::AUTHORIZATION, header_value);
                 Ok(req)
             }
-            Err(e) => Err(reqwest_middleware::Error::Middleware(anyhow::Error::new(e))),
+            Err(e) => Err(reqwest_middleware::Error::Middleware(anyhow::anyhow!(
+                "Failed to get GCS token: {:?}",
+                e
+            ))),
         },
         Err(e) => Err(reqwest_middleware::Error::Middleware(anyhow::Error::new(e))),
     }
