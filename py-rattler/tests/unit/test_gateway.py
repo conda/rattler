b856f114bebd24be2b4de51fd4205dbcff4d2336
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-11-05 16:06:36 +0100

    fix: repodata gateway recursive records

--- py-rattler/tests/unit/test_gateway.py
+++ py-rattler/tests/unit/test_gateway.py
@@ -2,13 +2,12 @@ import pytest
 
 from rattler import Gateway, Channel
 
+
 @pytest.mark.asyncio
 async def test_single_record_in_recursive_query(gateway: Gateway, conda_forge_channel: Channel) -> None:
-    records = await gateway.query(
-        [conda_forge_channel],
-        ["linux-64", "noarch"],
-        ["python 3.10.0 h543edf9_1_cpython"],
-        recursive=True)
+    subdirs = await gateway.query(
+        [conda_forge_channel], ["linux-64", "noarch"], ["python 3.10.0 h543edf9_1_cpython"], recursive=True
+    )
 
-    python_records = [record for record in records if record.name == "python"]
+    python_records = [record for subdir in subdirs for record in subdir if record.name == "python"]
     assert len(python_records) == 1
