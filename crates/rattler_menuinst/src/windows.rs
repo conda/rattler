88c2a5cb3952e2447efcc1a34c8472f9979982dc
Bas Zalmstra <bas@prefix.dev>
2024-11-09 19:25:36 +0100

    refactor: resolve later

--- crates/rattler_menuinst/src/windows.rs
+++ crates/rattler_menuinst/src/windows.rs
@@ -1,35 +1,50 @@
-use std::collections::HashMap;
-use std::fs::{self, File};
-use std::io::Write;
-use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::path::Path;
 
-mod knownfolders;
-mod registry;
+use crate::{
+    schema::{MenuItemCommand, Windows},
+    MenuInstError, MenuMode,
+};
 
-struct Directories {
-    start_menu_location: PathBuf,
-    quick_launch_location: PathBuf,
-    desktop_location: PathBuf,
-}
+// mod knownfolders;
+// mod registry;
+//
+// struct Directories {
+//     start_menu_location: PathBuf,
+//     quick_launch_location: PathBuf,
+//     desktop_location: PathBuf,
+// }
+//
+// impl Directories {
+//     pub fn create() -> Directories {
+//         let start_menu_location = dirs::start_menu_dir().unwrap();
+//         let quick_launch_location = dirs::quick_launch_dir().unwrap();
+//         let desktop_location = dirs::desktop_dir().unwrap();
+//
+//         Directories {
+//             start_menu_location,
+//             quick_launch_location,
+//             desktop_location,
+//         }
+//     }
+// }
+//
+// pub struct WindowsMenu {
+//     name: String,
+//     mode: String,
+//     prefix: PathBuf,
+//     base_prefix: PathBuf,
+// }
 
-impl Directories {
-    pub fn create() -> Directories {
-        let start_menu_location = dirs::start_menu_dir().unwrap();
-        let quick_launch_location = dirs::quick_launch_dir().unwrap();
-        let desktop_location = dirs::desktop_dir().unwrap();
-
-        Directories {
-            start_menu_location,
-            quick_launch_location,
-            desktop_location,
-        }
-    }
-}
-
-pub struct WindowsMenu {
-    name: String,
-    mode: String,
-    prefix: PathBuf,
-    base_prefix: PathBuf,
+pub(crate) fn install_menu_item(
+    prefix: &Path,
+    windows_item: Windows,
+    command: MenuItemCommand,
+    menu_mode: MenuMode,
+) -> Result<(), MenuInstError> {
+    // let bundle_name = macos_item.cf_bundle_name.as_ref().unwrap();
+    // let directories = Directories::new(menu_mode, bundle_name);
+    // println!("Installing menu item for {bundle_name}");
+    // let menu = crate::macos::MacOSMenu::new(prefix, macos_item, command,
+    // directories); menu.install()
+    Ok(())
 }
