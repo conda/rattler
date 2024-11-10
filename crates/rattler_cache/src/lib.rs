d36b4be72b872e3585335d0fa4e678cbbf153ce5
Hadrien Mary <hadrien.mary@gmail.com>
2024-11-10 18:42:14 -0500

    fix the join

--- crates/rattler_cache/src/lib.rs
+++ crates/rattler_cache/src/lib.rs
@@ -10,8 +10,8 @@ pub use consts::{PACKAGE_CACHE_DIR, REPODATA_CACHE_DIR};
 
 /// Determines the default cache directory for rattler.
 /// It first checks the environment variable `RATTLER_CACHE_DIR`.
-/// If not set, it falls back to the environment variable `XDG_CACHE_HOME`.
-/// If not set, it falls back to the standard cache directory provided by `dirs::cache_dir()`.
+/// If not set, it falls back to `XDG_CACHE_HOME/rattler/cache`.
+/// If not set, it falls back to the standard cache directory provided by `dirs::cache_dir()/rattler/cache`.
 pub fn default_cache_dir() -> anyhow::Result<PathBuf> {
     Ok(std::env::var("RATTLER_CACHE_DIR")
         .map(PathBuf::from)
@@ -23,6 +23,11 @@ pub fn default_cache_dir() -> anyhow::Result<PathBuf> {
                         anyhow::anyhow!("could not determine cache directory for current platform")
                     })
                 })
-        })?
-        .join("rattler/cache"))
+                // Append `rattler/cache` to the cache directory
+                .map(|mut p| {
+                    p.push("rattler");
+                    p.push("cache");
+                    p
+                })
+        })?)
 }
