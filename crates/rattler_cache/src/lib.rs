dd8579db28c213ff87098d84136d70f52971e5dd
Hadrien Mary <hadrien.mary@gmail.com>
2024-11-10 18:33:21 -0500

    Make the cache dir configurable

--- crates/rattler_cache/src/lib.rs
+++ crates/rattler_cache/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Ok;
 use std::path::PathBuf;
 
 pub mod package_cache;
@@ -7,9 +8,21 @@ pub mod validation;
 mod consts;
 pub use consts::{PACKAGE_CACHE_DIR, REPODATA_CACHE_DIR};
 
-/// Returns the default cache directory used by rattler.
+/// Determines the default cache directory for rattler.
+/// It first checks the environment variable `RATTLER_CACHE_DIR`.
+/// If not set, it falls back to the environment variable `XDG_CACHE_HOME`.
+/// If not set, it falls back to the standard cache directory provided by `dirs::cache_dir()`.
 pub fn default_cache_dir() -> anyhow::Result<PathBuf> {
-    Ok(dirs::cache_dir()
-        .ok_or_else(|| anyhow::anyhow!("could not determine cache directory for current platform"))?
+    Ok(std::env::var("RATTLER_CACHE_DIR")
+        .map(PathBuf::from)
+        .or_else(|_| {
+            std::env::var("XDG_CACHE_HOME")
+                .map(PathBuf::from)
+                .or_else(|_| {
+                    dirs::cache_dir().ok_or_else(|| {
+                        anyhow::anyhow!("could not determine cache directory for current platform")
+                    })
+                })
+        })?
         .join("rattler/cache"))
 }
