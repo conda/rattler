83253928cc36b168c9da88cb28ff76c7494c994b
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-11-16 21:59:10 +0100

    minor improvements

--- py-rattler/tests/unit/test_package_streaming.py
+++ py-rattler/tests/unit/test_package_streaming.py
@@ -47,7 +47,7 @@ async def test_download_from_oci(tmpdir: Path) -> None:
             # TODO somehow these URLs are very susceptible to missing last /
             # Maybe we can use the new ChannelURL type or one of these.
             MirrorMiddleware(
-                {"https://conda.anaconda.org/conda-forge/": ["oci://ghcr.io/channel-mirrors/conda-forge/"]}
+                {"https://conda.anaconda.org/conda-forge": ["oci://ghcr.io/channel-mirrors/conda-forge"]}
             ),
             OciMiddleware(),
         ]
