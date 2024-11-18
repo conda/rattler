ca549b9ff5616b610a28e252ec36fb6ebdcdd984
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-11-18 11:25:26 +0100

    feat: more setters for about.json and index.json  (#939)
    Adds a number of setters and tests.
    
    ---------
    
    Co-authored-by: Bas Zalmstra <bas@prefix.dev>

--- py-rattler/src/networking/middleware.rs
+++ py-rattler/src/networking/middleware.rs
@@ -1,6 +1,7 @@
 use pyo3::{pyclass, pymethods, FromPyObject, PyResult};
 use rattler_networking::{
     mirror_middleware::Mirror, AuthenticationMiddleware, AuthenticationStorage, MirrorMiddleware,
+    OciMiddleware,
 };
 use std::collections::HashMap;
 use url::Url;
@@ -9,8 +10,9 @@ use crate::error::PyRattlerError;
 
 #[derive(FromPyObject)]
 pub enum PyMiddleware {
-    MirrorMiddleware(PyMirrorMiddleware),
-    AuthenticationMiddleware(PyAuthenticationMiddleware),
+    Mirror(PyMirrorMiddleware),
+    Authentication(PyAuthenticationMiddleware),
+    Oci(PyOciMiddleware),
 }
 
 #[pyclass]
@@ -70,5 +72,24 @@ impl PyAuthenticationMiddleware {
 impl From<PyAuthenticationMiddleware> for AuthenticationMiddleware {
     fn from(_value: PyAuthenticationMiddleware) -> Self {
         AuthenticationMiddleware::new(AuthenticationStorage::default())
+    }
+}
+
+#[pyclass]
+#[repr(transparent)]
+#[derive(Clone)]
+pub struct PyOciMiddleware {}
+
+#[pymethods]
+impl PyOciMiddleware {
+    #[new]
+    pub fn __init__() -> Self {
+        Self {}
+    }
+}
+
+impl From<PyOciMiddleware> for OciMiddleware {
+    fn from(_value: PyOciMiddleware) -> Self {
+        OciMiddleware
     }
 }
