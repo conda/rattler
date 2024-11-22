9d4f01995bb22e729382836d3b813ace81b27ecc
Tim de Jager <tim@prefix.dev>
2024-11-22 15:12:06 +0100

    feat: fix typing

--- py-rattler/rattler/networking/middleware.py
+++ py-rattler/rattler/networking/middleware.py
@@ -109,8 +109,8 @@ class GCSMiddleware:
     Client()
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._middleware = PyGCSMiddleware()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{type(self).__name__}()"
