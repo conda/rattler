89758bcfa8e34f275ed557b00ef7038442afc80c
Pavel Zwerschke <pavelzw@gmail.com>
2025-01-05 20:34:31 +0100

    fix

--- crates/rattler/src/cli/auth.rs
+++ crates/rattler/src/cli/auth.rs
@@ -83,10 +83,6 @@ pub enum AuthenticationCLIError {
     #[error("Authentication with anaconda.org requires a conda token. Use `--conda-token` to provide one")]
     AnacondaOrgBadMethod,
 
-    /// S3 authentication needs an access key ID and a secret access key.
-    #[error("Secret access key must be provided when using S3 authentication")]
-    MissingSecretAccessKey,
-
     /// Bad authentication method when using S3
     #[error("Authentication with S3 requires a S3 access key ID and a secret access key. Use `--s3-access-key-id` and `--s3-secret-access-key` to provide them")]
     S3BadMethod,
@@ -130,17 +126,12 @@ fn login(args: LoginArgs, storage: AuthenticationStorage) -> Result<(), Authenti
         }
     } else if let Some(token) = args.token {
         Authentication::BearerToken(token)
-    } else if let Some(access_key_id) = args.s3_access_key_id {
-        if args.s3_secret_access_key.is_none() {
-            return Err(AuthenticationCLIError::MissingSecretAccessKey);
-        } else {
-            let secret_access_key = args.s3_secret_access_key.unwrap();
-            let session_token = args.s3_session_token;
-            Authentication::S3Credentials {
-                access_key_id,
-                secret_access_key,
-                session_token,
-            }
+    } else if let (Some(access_key_id), Some(secret_access_key)) = (args.s3_access_key_id, args.s3_secret_access_key) {
+        let session_token = args.s3_session_token;
+        Authentication::S3Credentials {
+            access_key_id,
+            secret_access_key,
+            session_token,
         }
     } else {
         return Err(AuthenticationCLIError::NoAuthenticationMethod);
