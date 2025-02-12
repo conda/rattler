2cfb9cb814076e511396aeab8823a8c436c87242
nichmor <nmorkotilo@gmail.com>
2025-02-12 11:08:16 +0200

    misc: add public exposed constant

--- crates/rattler_cache/src/consts.rs
+++ crates/rattler_cache/src/consts.rs
@@ -1,4 +1,5 @@
 /// The location in the main cache folder where the conda package cache is stored.
 pub const PACKAGE_CACHE_DIR: &str = "pkgs";
+pub const ARCHIVE_CACHE_DIR: &str = "archives";
 /// The location in the main cache folder where the repodata cache is stored.
 pub const REPODATA_CACHE_DIR: &str = "repodata";
