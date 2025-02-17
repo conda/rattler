7561337c0f72041b2b077051f24f33def482e64f
Travis Hathaway <travis.j.hathaway@gmail.com>
2025-02-17 19:07:00 -0300

    removing unused modules

--- crates/rattler_pypi_interop/src/lib.rs
+++ crates/rattler_pypi_interop/src/lib.rs
@@ -1,12 +1,8 @@
 // Private modules
 mod utils;
-// mod win;
 
 // Public modules
 pub mod index;
-// pub mod resolve;
-// pub mod wheel_builder;
 pub mod artifacts;
 pub mod python_env;
-// pub mod install; 
 pub mod types;
