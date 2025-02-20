ab1b0a31ceb34bcec9b0097324602e019a1505a2
Daniel Elsner <daniel.elsner@quantco.com>
2025-02-20 14:32:34 +0100

    Fixes

--- crates/rattler_index/tests/test_s3.rs
+++ crates/rattler_index/tests/test_s3.rs
@@ -2,7 +2,7 @@ use assert_cmd::cargo::CommandCargoExt;
 use std::process::Command;
 
 #[tokio::test]
-async fn test_cli() {
+async fn test_s3() {
     let mut cmd = Command::cargo_bin("rattler-index").unwrap();
     let args = ["--verbose", "fs", "."];
 
