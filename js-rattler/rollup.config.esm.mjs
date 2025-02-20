6f79ba7b444a20c012bb8f99b695d57badc5ebaf
Trim21 <trim21.me@gmail.com>
2025-02-21 04:24:13 +0800

    we do not need a build script

--- js-rattler/rollup.config.esm.mjs
+++ js-rattler/rollup.config.esm.mjs
@@ -6,7 +6,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-    input: "src/index.ts",
+    input: "src/esm.ts",
     output: {
         file: "dist/rattler.bundler.mjs",
         sourcemap: false,
