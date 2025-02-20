58d43c6a2870052bbc54a8de93910fc7d4144ac1
Trim21 <trim21.me@gmail.com>
2025-02-20 16:26:19 +0800

    Update js-rattler/src/Version.ts

--- js-rattler/src/Version.ts
+++ js-rattler/src/Version.ts
@@ -199,7 +199,7 @@ export class Version {
     /**
      * Compare two versions.
      *
-     * Returns `-1` if this<other, `0` if this == other, `1` if this > other
+     * Returns `-1` if this < other, `0` if this == other, `1` if this > other
      */
     public compare(other: Version): number {
         return this.native.compare(other.native);
