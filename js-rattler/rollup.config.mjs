d5766a8dba9f2dbdc1080d7afbe18afef6370a0c
Trim21 <trim21.me@gmail.com>
2025-02-21 01:46:13 +0800

    fix

--- js-rattler/rollup.config.mjs
+++ js-rattler/rollup.config.mjs
@@ -1,56 +1,57 @@
-/**
- * build node distribution
- */
+/** Build node distribution */
 
 import { wasm } from "@rollup/plugin-wasm";
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import esmShim from '@rollup/plugin-esm-shim';
+import esmShim from "@rollup/plugin-esm-shim";
 
-export default [{
-    input: "src/index.ts",
-    output: {
-        file: "dist/rattler.node.mjs",
-        sourcemap: false,
-        format: "esm",
+export default [
+    {
+        input: "src/index.ts",
+        output: {
+            file: "dist/rattler.node.mjs",
+            sourcemap: false,
+            format: "esm",
+        },
+        plugins: [
+            esmShim(),
+            commonjs(),
+            wasm({
+                targetEnv: "auto-inline",
+                sync: ["pkg/js_rattler_bg.wasm"],
+            }),
+            nodeResolve(),
+            typescript({
+                sourceMap: false,
+                declaration: false,
+                declarationMap: false,
+                inlineSources: false,
+                tsconfig: "./tsconfig.rollup.json",
+            }),
+        ],
     },
-    plugins: [
-        esmShim(),
-        commonjs(),
-        wasm({
-            targetEnv: "auto-inline",
-            sync: ["pkg/js_rattler_bg.wasm"],
-        }),
-        nodeResolve(),
-        typescript({
-            sourceMap: false,
-            declaration: false,
-            declarationMap: false,
-            inlineSources: false,
-            tsconfig: "./tsconfig.rollup.json",
-        }),
-    ],
-}, {
-    input: "src/index.ts",
-    output: {
-        file: "dist/rattler.node.cjs",
-        sourcemap: false,
-        format: "commonjs",
+    {
+        input: "src/index.ts",
+        output: {
+            file: "dist/rattler.node.cjs",
+            sourcemap: false,
+            format: "commonjs",
+        },
+        plugins: [
+            commonjs(),
+            wasm({
+                targetEnv: "auto-inline",
+                sync: ["pkg/js_rattler_bg.wasm"],
+            }),
+            nodeResolve(),
+            typescript({
+                sourceMap: false,
+                declaration: false,
+                declarationMap: false,
+                inlineSources: false,
+                tsconfig: "./tsconfig.rollup.json",
+            }),
+        ],
     },
-    plugins: [
-        commonjs(),
-        wasm({
-            targetEnv: "auto-inline",
-            sync: ["pkg/js_rattler_bg.wasm"],
-        }),
-        nodeResolve(),
-        typescript({
-            sourceMap: false,
-            declaration: false,
-            declarationMap: false,
-            inlineSources: false,
-            tsconfig: "./tsconfig.rollup.json",
-        }),
-    ],
-}];
+];
