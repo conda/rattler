232faacf38e84295083f143ef48af6fb9adf25fa
Trim21 <trim21.me@gmail.com>
2025-02-21 01:44:51 +0800

    fix nodejs esm

--- js-rattler/rollup.config.mjs
+++ js-rattler/rollup.config.mjs
@@ -1,9 +1,37 @@
+/**
+ * build node distribution
+ */
+
 import { wasm } from "@rollup/plugin-wasm";
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import esmShim from '@rollup/plugin-esm-shim';
 
-export default {
+export default [{
+    input: "src/index.ts",
+    output: {
+        file: "dist/rattler.node.mjs",
+        sourcemap: false,
+        format: "esm",
+    },
+    plugins: [
+        esmShim(),
+        commonjs(),
+        wasm({
+            targetEnv: "auto-inline",
+            sync: ["pkg/js_rattler_bg.wasm"],
+        }),
+        nodeResolve(),
+        typescript({
+            sourceMap: false,
+            declaration: false,
+            declarationMap: false,
+            inlineSources: false,
+            tsconfig: "./tsconfig.rollup.json",
+        }),
+    ],
+}, {
     input: "src/index.ts",
     output: {
         file: "dist/rattler.node.cjs",
@@ -25,4 +53,4 @@ export default {
             tsconfig: "./tsconfig.rollup.json",
         }),
     ],
-};
+}];
