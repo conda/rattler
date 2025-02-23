68f1b1d89c35358e5d71067a8407bac4cc722117
Travis Hathaway <travis.j.hathaway@gmail.com>
2025-02-23 19:00:35 -0300

    adding a new example

--- crates/rattler_pypi_interop/src/artifacts/mod.rs
+++ crates/rattler_pypi_interop/src/artifacts/mod.rs
@@ -1,4 +1,31 @@
-//! Module containing artifacts that can be resolved and installed.
+//! Module containing data types for working with Python packaging artifacts.
+//!
+//! This module can be used to read and parse Python packaging artifacts like wheels
+//! (binary distribution) and sdists (source distribution).
+//!
+//! Example of reading PKG-INFO from an sdist file:
+//!
+//! ```rust
+//! use std::str::FromStr;
+//! use std::path::PathBuf;
+//! use rattler_pypi_interop::artifacts::SDist;
+//! use rattler_pypi_interop::types::NormalizedPackageName;
+//!
+//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR")?;
+//!
+//!     let test_data_dir = PathBuf::from(manifest_dir).join(PathBuf::from("test-data/sdists"));
+//!     let path_to_wheel = test_data_dir.join(PathBuf::from("rich-13.6.0.tar.gz"));
+//!     let normalized_package_name = NormalizedPackageName::from_str("rich")?;
+//!
+//!     let sdist = SDist::from_path(&path_to_wheel, &normalized_package_name)?;
+//!     let (bytes, package_info) = sdist.read_package_info()?;
+//!     println!("Length of bytes: {}", bytes.len());
+//!     println!("{:?}", package_info.parsed.fields);
+//!
+//!     Ok(())
+//! }
+//! ```
 //!
 mod sdist;
 mod stree;
