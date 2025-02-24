f81bed683abeb3501d6b6d5a08980603eda50523
Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>
2025-02-24 15:59:06 +0100

    fix: compiling with pixi on linux (#1097)
    * fix: compiling with pixi on linux
    
    I mostly had to switch from native-tls to rust-tls.
    
    Annoyingly, I didn't manage to convince the build-process to find libclang needed to compile libsolv so I skip that test for now
    
    * Update pixi.toml

--- scripts/activate_linux.sh
+++ scripts/activate_linux.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+# Ignore requiring a shebang as this is a script meant to be sourced
+# shellcheck disable=SC2148
+
+# Setup the mold linker when targeting x86_64-unknown-linux-gnu
 set -Eeuo pipefail
 export CARGO_TARGET_DIR=".pixi/target"
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang"
