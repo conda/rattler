ebcca594b024d7da49e4069796e9e763113cff81
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-02-27 10:42:29 +0100

    remove unused file

--- crates/rattler_shell/src/lib.rs
+++ crates/rattler_shell/src/lib.rs
@@ -1,9 +1,7 @@
+//! This crate provides helper functions to activate and deactivate virtual environments.
 #![deny(missing_docs)]
 
-//! This crate provides helper functions to activate and deactivate virtual environments.
-
 pub mod activation;
-pub mod completion;
 pub mod run;
 pub mod shell;
 pub use run::run_in_environment;
