d39bf1e3c4f27f3e035be00cdbd4d817b210e080
Bas Zalmstra <zalmstra.bas@gmail.com>
2025-03-11 16:54:47 +0100

    fix: build issues

--- js-rattler/crate/solve.rs
+++ js-rattler/crate/solve.rs
@@ -1,19 +1,23 @@
-use std::{path::PathBuf, str::FromStr};
+use std::path::PathBuf;
 
-use rattler_conda_types::{Channel, ChannelConfig, MatchSpec, ParseStrictness::Lenient, Platform};
+use rattler_conda_types::{Channel, ChannelConfig, MatchSpec, ParseStrictness::Lenient};
 use rattler_repodata_gateway::{Gateway, SourceConfig};
 use rattler_solve::{SolverImpl, SolverTask};
 use wasm_bindgen::prelude::*;
 
-use crate::error::JsError;
+use crate::{error::JsError, platform::JsPlatform};
 
 /// A package that has been solved by the solver.
 /// @public
 #[wasm_bindgen(getter_with_clone)]
 pub struct SolvedPackage {
     pub url: String,
+    #[wasm_bindgen(js_name = "packageName")]
     pub package_name: String,
+    pub build: String,
+    #[wasm_bindgen(js_name = "buildNumber")]
     pub build_number: u64,
+    #[wasm_bindgen(js_name = "repoName")]
     pub repo_name: Option<String>,
     pub filename: String,
     pub version: String,
@@ -21,11 +25,14 @@ pub struct SolvedPackage {
 
 /// Solve a set of specs with the given channels and platforms.
 /// @public
-#[wasm_bindgen]
+#[wasm_bindgen(js_name = "simpleSolve")]
 pub async fn simple_solve(
-    specs: Vec<String>,
+    #[wasm_bindgen(param_description = "Matchspecs of packages that must be included.")] specs: Vec<
+        String,
+    >,
+    #[wasm_bindgen(param_description = "The channels to request for repodata of packages")]
     channels: Vec<String>,
-    platforms: Vec<String>,
+    #[wasm_bindgen(param_description = "The platforms to solve for")] platforms: Vec<JsPlatform>,
 ) -> Result<Vec<SolvedPackage>, JsError> {
     // TODO: Dont hardcode
     let channel_config = ChannelConfig::default_with_root_dir(PathBuf::from(""));
@@ -41,7 +48,7 @@ pub async fn simple_solve(
         .collect::<Result<Vec<_>, _>>()?;
     let platforms = platforms
         .into_iter()
-        .map(|p| Platform::from_str(&p))
+        .map(|p| serde_wasm_bindgen::from_value(p.into()))
         .collect::<Result<Vec<_>, _>>()?;
 
     // Fetch the repodata
@@ -74,6 +81,7 @@ pub async fn simple_solve(
         .map(|r| SolvedPackage {
             url: r.url.to_string(),
             package_name: r.package_record.name.as_source().to_string(),
+            build: r.package_record.build.clone(),
             build_number: r.package_record.build_number,
             repo_name: r.channel,
             filename: r.file_name,
