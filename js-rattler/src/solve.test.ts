a13a1eafb75209127e1d63c36ee773ec2529f232
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-03-26 08:16:06 +0100

    fix test for now

--- js-rattler/src/solve.test.ts
+++ js-rattler/src/solve.test.ts
@@ -14,7 +14,7 @@ describe("solving", () => {
             expect(result.map((pkg) => pkg.url).sort()).toStrictEqual([
                 "https://prefix.dev/emscripten-forge-dev/emscripten-wasm32/python-3.13.1-h_dd8ba0c_4_cp313.conda",
                 "https://prefix.dev/emscripten-forge-dev/emscripten-wasm32/python_abi-3.13.1-0_cp313.tar.bz2",
-                "https://prefix.dev/emscripten-forge-dev/noarch/emscripten-abi-3.1.73-h267e887_6.tar.bz2",
+                "https://prefix.dev/emscripten-forge-dev/noarch/emscripten-abi-3.1.73-h267e887_7.tar.bz2",
             ]),
         );
     });
