36e3fbfc7a5dc766e6336e186fcd92f5fa4cf004
Bas Zalmstra <zalmstra.bas@gmail.com>
2025-04-03 17:21:24 +0200

    test: parse sources

--- crates/rattler_lock/src/lib.rs
+++ crates/rattler_lock/src/lib.rs
@@ -548,6 +548,7 @@ mod test {
     #[case::v5_with_and_without_purl("v5/similar-with-and-without-purl.yml")]
     #[case::v6_conda_source_path("v6/conda-path-lock.yml")]
     #[case::v6_derived_channel("v6/derived-channel-lock.yml")]
+    #[case::v6_sources("v6/sources-lock.yml")]
     fn test_parse(#[case] file_name: &str) {
         let path = Path::new(env!("CARGO_MANIFEST_DIR"))
             .join("../../test-data/conda-lock")
