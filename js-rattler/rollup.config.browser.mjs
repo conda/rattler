cdc7112d5f74b57dd88e14208653dacc529ef873
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-04-09 13:57:05 +0200

    fix style

--- js-rattler/rollup.config.browser.mjs
+++ js-rattler/rollup.config.browser.mjs
@@ -4,26 +4,26 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "src/esm.ts",
-  output: {
-    file: "dist/rattler.browser.js",
-    format: "iife",
-    name: "Rattler", // This will be the global variable name
-    sourcemap: true,
-  },
-  plugins: [
-    commonjs(),
-    wasm({
-      targetEnv: "auto-inline",
-      sync: ["pkg/js_rattler_bg.wasm"],
-    }),
-    nodeResolve(),
-    typescript({
-      sourceMap: true,
-      declaration: false,
-      declarationMap: false,
-      inlineSources: false,
-      tsconfig: "./tsconfig.rollup.json",
-    }),
-  ],
+    input: "src/esm.ts",
+    output: {
+        file: "dist/rattler.browser.js",
+        format: "iife",
+        name: "Rattler", // This will be the global variable name
+        sourcemap: true,
+    },
+    plugins: [
+        commonjs(),
+        wasm({
+            targetEnv: "auto-inline",
+            sync: ["pkg/js_rattler_bg.wasm"],
+        }),
+        nodeResolve(),
+        typescript({
+            sourceMap: true,
+            declaration: false,
+            declarationMap: false,
+            inlineSources: false,
+            tsconfig: "./tsconfig.rollup.json",
+        }),
+    ],
 };
