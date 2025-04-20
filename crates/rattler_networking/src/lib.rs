c2458b4aa79ce5669418b31653d449f0f02256cf
priyaa1110 <priiyaaa1102@gmail.com>
2025-04-20 12:25:16 +0530

    refactor: Keep only mirror middleware implementation and dependencies

--- crates/rattler_networking/src/lib.rs
+++ crates/rattler_networking/src/lib.rs
@@ -1,24 +1,5 @@
 #![deny(missing_docs)]
 
-//! Networking utilities for Rattler, specifically authenticating requests
-pub use authentication_middleware::AuthenticationMiddleware;
-pub use authentication_storage::{authentication::Authentication, storage::AuthenticationStorage};
+//! Networking utilities for Rattler, specifically for handling mirrors
 pub use mirror_middleware::MirrorMiddleware;
-pub use oci_middleware::OciMiddleware;
-
-#[cfg(feature = "gcs")]
-pub mod gcs_middleware;
-#[cfg(feature = "gcs")]
-pub use gcs_middleware::GCSMiddleware;
-
-#[cfg(feature = "s3")]
-pub mod s3_middleware;
-#[cfg(feature = "s3")]
-pub use s3_middleware::S3Middleware;
-
-pub mod authentication_middleware;
-pub mod authentication_storage;
-
 pub mod mirror_middleware;
-pub mod oci_middleware;
-pub mod retry_policies;
