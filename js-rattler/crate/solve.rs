4171a4b4d540d3e0c7c1185aee53eeb327bfbc2a
Anastasiia Sliusar <anastasiia.sliusar@gmail.com>
2025-05-19 17:43:57 +0200

    fix: Add filtering packages

--- js-rattler/crate/solve.rs
+++ js-rattler/crate/solve.rs
@@ -1,20 +1,18 @@
 use std::path::PathBuf;
 use std::str::FromStr;
 
+use crate::{error::JsError, platform::JsPlatform};
 use rattler_conda_types::{
-    ParseChannelError, Channel, ChannelConfig, MatchSpec, ParseStrictness::Lenient, PackageName, Version, RepoDataRecord, PackageRecord,
+    Channel, ChannelConfig, MatchSpec, NoArchType, PackageName, PackageRecord, ParseChannelError,
+    ParseStrictness::Lenient, RepoDataRecord, Version,
 };
 use rattler_repodata_gateway::{Gateway, SourceConfig};
 use rattler_solve::{SolverImpl, SolverTask};
-use wasm_bindgen::prelude::*;
-
-use crate::{error::JsError, platform::JsPlatform};
-
-use web_sys::console::log_1;
-use serde::{Serialize, Deserialize};
-
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+use std::collections::HashSet;
 use url::Url;
-
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(getter_with_clone)]
 #[derive(Serialize)]
@@ -31,8 +29,7 @@ pub struct SolvedPackage {
     pub version: String,
 }
 
-
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 struct JsLockedPackage {
     url: String,
@@ -40,7 +37,9 @@ struct JsLockedPackage {
     build: String,
     repo_name: Option<String>,
     filename: String,
-    version: String
+    version: String,
+    depends: Option<Vec<String>>,
+    build_number: u64,
 }
 
 /// Solve a set of specs with the given channels and platforms.
@@ -89,40 +88,70 @@ pub async fn simple_solve(
         .execute()
         .await?;
 
-  
-  let installed_packages: Option<Vec<RepoDataRecord>> = if locked_packages.is_null() || locked_packages.is_undefined() {
-        None
-    } else {
-        let js_locked_packages: Vec<JsLockedPackage> =
-            serde_wasm_bindgen::from_value(locked_packages).map_err(JsError::from)?;
+    let (installed_packages, installed_filter_keys): (Option<Vec<RepoDataRecord>>, HashSet<_>) =
+        if locked_packages.is_null() || locked_packages.is_undefined() {
+            (None, HashSet::new())
+        } else {
+            let js_locked_packages: Vec<JsLockedPackage> =
+                serde_wasm_bindgen::from_value(locked_packages).map_err(JsError::from)?;
+            let js_locked_packages_for_filter = js_locked_packages.clone();
+            let records = js_locked_packages
+                .into_iter()
+                .map(|pkg| {
+                    let url = Url::parse(&pkg.url)
+                        .map_err(|e| JsError::from(ParseChannelError::from(e)))?;
 
-        let records = js_locked_packages
-            .into_iter()
-            .map(|pkg| {
-                let url = Url::parse(&pkg.url)
-                    .map_err(|e| JsError::from(ParseChannelError::from(e)))?;
+                    let rec = PackageRecord {
+                        name: PackageName::try_from(pkg.package_name.clone())?,
+                        version: Version::from_str(&pkg.version)?.into(),
+                        build: pkg.build.clone(),
+                        build_number: pkg.build_number,
+                        subdir: "unknown".to_string(),
+                        md5: None,
+                        sha256: None,
+                        size: None,
+                        arch: None,
+                        platform: None,
+                        depends: pkg.depends.unwrap_or_default(),
+                        extra_depends: BTreeMap::new(),
+                        constrains: vec![],
+                        track_features: vec![],
+                        features: None,
+                        noarch: NoArchType::none(),
+                        license: None,
+                        license_family: None,
+                        timestamp: None,
+                        python_site_packages_path: None,
+                        legacy_bz2_md5: None,
+                        legacy_bz2_size: None,
+                        purls: None,
+                        run_exports: None,
+                    };
 
-                  let rec = PackageRecord {
-                    ..PackageRecord::new(
-                        PackageName::try_from(pkg.package_name.clone())?,
-                        Version::from_str(&pkg.version)?,
-                        pkg.build.clone(),
-                    )
-                };
-
-                Ok(RepoDataRecord {
-                    url,
-                    file_name: pkg.filename,
-                    channel: pkg.repo_name,
-                    package_record:rec.clone()
+                    Ok(RepoDataRecord {
+                        url,
+                        file_name: pkg.filename,
+                        channel: pkg.repo_name,
+                        package_record: rec.clone(),
+                    })
                 })
-            })
-            .collect::<Result<Vec<_>, JsError>>()?;
+                .collect::<Result<Vec<_>, JsError>>()?;
 
-        Some(records)
-    };
+            let installed_filter_keys: HashSet<_> = js_locked_packages_for_filter
+                .into_iter()
+                .map(|pkg| {
+                    Ok((
+                        PackageName::try_from(pkg.package_name.clone())?,
+                        Version::from_str(&pkg.version)?.into(),
+                        pkg.build.clone(),
+                    ))
+                })
+                .collect::<Result<_, JsError>>()?;
 
-        let task = SolverTask {
+            (Some(records), installed_filter_keys)
+        };
+
+    let task = SolverTask {
         specs,
         locked_packages: installed_packages.unwrap_or_default(),
         pinned_packages: vec![],
@@ -131,12 +160,17 @@ pub async fn simple_solve(
 
     let solved = rattler_solve::resolvo::Solver.solve(task)?;
 
-    let js_value = serde_wasm_bindgen::to_value(&solved.records).unwrap();
-    log_1(&js_value);
-
     Ok(solved
         .records
         .into_iter()
+        .filter(|r| {
+            let key = (
+                r.package_record.name.clone(),
+                r.package_record.version.clone(),
+                r.package_record.build.clone(),
+            );
+            !installed_filter_keys.contains(&key)
+        })
         .map(|r| SolvedPackage {
             url: r.url.to_string(),
             package_name: r.package_record.name.as_source().to_string(),
