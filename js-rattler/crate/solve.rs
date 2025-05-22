bdb6c58ce2a14e7c9dadd5ada883df56dcc8e835
Anastasiia Sliusar <anastasiia.sliusar@gmail.com>
2025-05-22 11:28:47 +0200

    feat: add skipping logic

--- js-rattler/crate/solve.rs
+++ js-rattler/crate/solve.rs
@@ -4,7 +4,7 @@ use std::str::FromStr;
 use crate::{error::JsError, platform::JsPlatform};
 use rattler_conda_types::{
     Channel, ChannelConfig, MatchSpec, NoArchType, PackageName, PackageRecord, ParseChannelError,
-    ParseStrictness::Lenient, RepoDataRecord, Version,
+    ParseStrictness::Lenient, RepoDataRecord, Version, Matches,
 };
 use rattler_repodata_gateway::{Gateway, SourceConfig};
 use rattler_solve::{SolverImpl, SolverTask};
@@ -27,6 +27,8 @@ pub struct SolvedPackage {
     pub repo_name: Option<String>,
     pub filename: String,
     pub version: String,
+    pub depends: Vec<String>,
+    pub subdir: String,
 }
 
 #[derive(Deserialize, Clone)]
@@ -40,6 +42,7 @@ struct JsLockedPackage {
     version: String,
     depends: Option<Vec<String>>,
     build_number: u64,
+    subdir: Option<String>,
 }
 
 /// Solve a set of specs with the given channels and platforms.
@@ -71,35 +74,6 @@ pub async fn simple_solve(
         .map(|p| serde_wasm_bindgen::from_value(p.into()))
         .collect::<Result<Vec<_>, _>>()?;
 
-    // Fetch the repodata
-    let gateway = Gateway::builder()
-        .with_channel_config(rattler_repodata_gateway::ChannelConfig {
-            default: SourceConfig {
-                sharded_enabled: true,
-                ..SourceConfig::default()
-            },
-            ..rattler_repodata_gateway::ChannelConfig::default()
-        })
-        .finish();
-
-    let repodata = gateway
-        .query(channels, platforms, specs.iter().cloned())
-        .recursive(true)
-        .execute()
-        .await?;
-
-    // We need this to find depends for locked packages
-    let repodata_keys: HashMap<(String, String, &String), &Vec<String>> = repodata
-        .iter()
-        .flat_map(|r| r.iter())
-        .map(|rec| {
-            let name = rec.package_record.name.as_normalized().to_string();
-            let version = rec.package_record.version.to_string();
-            let build = &rec.package_record.build;
-            ((name, version, build), &rec.package_record.depends)
-        })
-        .collect();
-
     let mut installed_packages: Vec<RepoDataRecord> =
         if locked_packages.is_null() || locked_packages.is_undefined() {
             vec![]
@@ -118,13 +92,13 @@ pub async fn simple_solve(
                         version: Version::from_str(&pkg.version)?.into(),
                         build: pkg.build.clone(),
                         build_number: pkg.build_number,
-                        subdir: "unknown".to_string(),
                         md5: None,
                         sha256: None,
                         size: None,
                         arch: None,
                         platform: None,
                         depends: pkg.depends.unwrap_or_default(),
+                        subdir:pkg.subdir.unwrap_or_else(|| "unknown".to_string()),
                         extra_depends: BTreeMap::new(),
                         constrains: vec![],
                         track_features: vec![],
@@ -152,40 +126,88 @@ pub async fn simple_solve(
             records
         };
 
-    // if a locked package does not include depends then depends will be taken from repodata
-    for records in installed_packages.iter_mut() {
-        let key = (
-            records.package_record.name.as_normalized().to_string(),
-            records.package_record.version.to_string(),
-            &records.package_record.build,
-        );
+    //if we do not need to solve the same packages, then filter them
+    let filtered_specs: Vec<MatchSpec> = specs.clone()
+    .into_iter()
+    .filter(|spec| {
+        let matched = installed_packages
+            .iter()
+            .any(|rec| spec.matches(&rec.package_record));
 
-        if records.package_record.depends.is_empty() {
-            if let Some(deps) = repodata_keys.get(&key) {
-                records.package_record.depends = deps.to_vec();
+        !matched
+    })
+    .collect();
+
+
+    if filtered_specs.is_empty() {
+        Ok(vec![])
+    } else {
+        // Fetch the repodata
+        let gateway = Gateway::builder()
+            .with_channel_config(rattler_repodata_gateway::ChannelConfig {
+                default: SourceConfig {
+                    sharded_enabled: true,
+                    ..SourceConfig::default()
+                },
+                ..rattler_repodata_gateway::ChannelConfig::default()
+            })
+            .finish();
+
+        let repodata = gateway
+            .query(channels, platforms, specs.iter().cloned())
+            .recursive(true)
+            .execute()
+            .await?;
+
+        // We need this to find depends for locked packages
+        let repodata_keys: HashMap<(String, String, &String), &Vec<String>> = repodata
+            .iter()
+            .flat_map(|r| r.iter())
+            .map(|rec| {
+                let name = rec.package_record.name.as_normalized().to_string();
+                let version = rec.package_record.version.to_string();
+                let build = &rec.package_record.build;
+                ((name, version, build), &rec.package_record.depends)
+            })
+            .collect();
+
+        // if a locked package does not include depends then depends will be taken from repodata
+        for records in installed_packages.iter_mut() {
+            let key = (
+                records.package_record.name.as_normalized().to_string(),
+                records.package_record.version.to_string(),
+                &records.package_record.build,
+            );
+
+            if records.package_record.depends.is_empty() {
+                if let Some(deps) = repodata_keys.get(&key) {
+                    records.package_record.depends = deps.to_vec();
+                }
             }
         }
+
+        let task = SolverTask {
+            specs,
+            locked_packages: installed_packages,
+            ..repodata.iter().collect::<SolverTask<_>>()
+        };
+
+        let solved = rattler_solve::resolvo::Solver.solve(task)?;
+
+        Ok(solved
+            .records
+            .into_iter()
+            .map(|r| SolvedPackage {
+                url: r.url.to_string(),
+                package_name: r.package_record.name.as_source().to_string(),
+                build: r.package_record.build.clone(),
+                build_number: r.package_record.build_number,
+                repo_name: r.channel,
+                filename: r.file_name,
+                version: r.package_record.version.to_string(),
+                depends: r.package_record.depends,
+                subdir: r.package_record.subdir
+            })
+            .collect())
     }
-
-    let task = SolverTask {
-        specs,
-        locked_packages: installed_packages,
-        ..repodata.iter().collect::<SolverTask<_>>()
-    };
-
-    let solved = rattler_solve::resolvo::Solver.solve(task)?;
-
-    Ok(solved
-        .records
-        .into_iter()
-        .map(|r| SolvedPackage {
-            url: r.url.to_string(),
-            package_name: r.package_record.name.as_source().to_string(),
-            build: r.package_record.build.clone(),
-            build_number: r.package_record.build_number,
-            repo_name: r.channel,
-            filename: r.file_name,
-            version: r.package_record.version.to_string(),
-        })
-        .collect())
 }
