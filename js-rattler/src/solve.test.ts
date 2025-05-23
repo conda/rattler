7477599267ec271eea608908921fe6e4c0c1e095
Anastasiia Sliusar <anastasiia.sliusar@gmail.com>
2025-05-23 10:50:13 +0200

    fix: add tests for cases when there is skipping solving packages and when it is not present

--- js-rattler/src/solve.test.ts
+++ js-rattler/src/solve.test.ts
@@ -26,4 +26,79 @@ describe("solving", () => {
             });
         });
     });
+
+    it("python should yield three packages but numpy should not be solved again", () => {
+        let big = 1n;
+        let buildNumber = Number(big);
+        return simpleSolve(
+            ["python", "numpy"],
+            [
+                "https://prefix.dev/emscripten-forge-dev",
+                "https://prefix.dev/conda-forge",
+            ],
+            ["emscripten-wasm32", "noarch"],
+            [
+                {
+                    build: "py313h6394566_1",
+                    buildNumber,
+                    filename: "numpy-2.2.6-py313h6394566_1.tar.bz2",
+                    packageName: "numpy",
+                    repoName: "https://prefix.dev/emscripten-forge-dev/",
+                    url: "https://prefix.dev/emscripten-forge-dev/emscripten-wasm32/numpy-2.2.6-py313h6394566_1.tar.bz2",
+                    version: "2.2.6",
+                },
+            ],
+        ).then((result) => {
+            const expectedPrefixes = [
+                "https://prefix.dev/emscripten-forge-dev/emscripten-wasm32/python-",
+                "https://prefix.dev/emscripten-forge-dev/emscripten-wasm32/python_abi-",
+                "https://prefix.dev/emscripten-forge-dev/noarch/emscripten-abi-",
+            ];
+
+            const urls = result.map((pkg) => pkg.url).sort();
+            expect(urls.length).toBe(expectedPrefixes.length);
+
+            urls.forEach((url, index) => {
+                expect(url.startsWith(expectedPrefixes[index])).toBe(true);
+            });
+        });
+    });
+
+    it("there should be 4 packages with python and numpy", () => {
+        let big = 1n;
+        let buildNumber = Number(big);
+        return simpleSolve(
+            ["python", "numpy<2.2.6"],
+            [
+                "https://prefix.dev/emscripten-forge-dev",
+                "https://prefix.dev/conda-forge",
+            ],
+            ["emscripten-wasm32", "noarch"],
+            [
+                {
+                    build: "py313h6394566_1",
+                    buildNumber,
+                    filename: "numpy-2.2.6-py313h6394566_1.tar.bz2",
+                    packageName: "numpy",
+                    repoName: "https://prefix.dev/emscripten-forge-dev/",
+                    url: "https://prefix.dev/emscripten-forge-dev/emscripten-wasm32/numpy-2.2.6-py313h6394566_1.tar.bz2",
+                    version: "2.2.6",
+                },
+            ],
+        ).then((result) => {
+            const expectedPrefixes = [
+                "https://prefix.dev/emscripten-forge-dev/emscripten-wasm32/numpy-",
+                "https://prefix.dev/emscripten-forge-dev/emscripten-wasm32/python-",
+                "https://prefix.dev/emscripten-forge-dev/emscripten-wasm32/python_abi-",
+                "https://prefix.dev/emscripten-forge-dev/noarch/emscripten-abi-",
+            ];
+
+            const urls = result.map((pkg) => pkg.url).sort();
+            expect(urls.length).toBe(expectedPrefixes.length);
+
+            urls.forEach((url, index) => {
+                expect(url.startsWith(expectedPrefixes[index])).toBe(true);
+            });
+        });
+    });
 });
