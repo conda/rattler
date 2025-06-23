550e1119fbfe53486d999d2f786005be82d0566e
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-06-23 13:27:07 +0200

    cleanup

--- crates/rattler_config/src/edit.rs
+++ crates/rattler_config/src/edit.rs
@@ -1,5 +1,4 @@
-use std::path::Path;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use crate::config::{Config, ConfigBase};
 use serde::de::DeserializeOwned;
@@ -67,9 +66,7 @@ where
 {
     /// Modify this config with the given key and value
     ///
-    /// # Note
-    ///
-    /// It is required to call `save()` to persist the changes.
+    /// It is required to call `save()` to persist the changes on disk.
     pub fn set(&mut self, key: &str, value: Option<String>) -> Result<(), ConfigEditError> {
         let get_supported_keys = |config: &Self| config.keys().join(",\n\t");
 
@@ -113,29 +110,17 @@ where
                     .unwrap_or_default();
                 Ok(())
             }
-            // "detached-environments" => {
-            //     self.detached_environments = value.map(|v| match v.as_str() {
-            //         "true" => DetachedEnvironments::Boolean(true),
-            //         "false" => DetachedEnvironments::Boolean(false),
-            //         _ => DetachedEnvironments::Path(PathBuf::from(v)),
-            //     });
-            // }
-            // "pinning-strategy" => {
-            //     self.pinning_strategy = value
-            //         .map(|v| PinningStrategy::from_str(v.as_str()))
-            //         .transpose()
-            //         .into_diagnostic()?
-            // }
-            // "change-ps1" => {
-            //     return Err(miette::miette!(
-            //         "The `change-ps1` field is deprecated. Please use the `shell.change-ps1` field instead."
-            //     ));
-            // }
-            // "force-activate" => {
-            //     return Err(miette::miette!(
-            //         "The `force-activate` field is deprecated. Please use the `shell.force-activate` field instead."
-            //     ));
-            // }
+            "run-post-link-scripts" => {
+                let value = value.ok_or_else(|| ConfigEditError::MissingValue {
+                    key: key.to_string(),
+                })?;
+                self.run_post_link_scripts =
+                    Some(value.parse().map_err(|e| ConfigEditError::InvalidValue {
+                        key: key.to_string(),
+                        source: Box::new(e),
+                    })?);
+                Ok(())
+            }
             key if key.starts_with("repodata-config") => {
                 self.repodata_config.set(key, value)?;
                 Ok(())
@@ -145,50 +130,7 @@ where
                 Ok(())
             }
             key if key.starts_with("concurrency.") => {
-                let subkey = key.strip_prefix("concurrency.").unwrap();
-                match subkey {
-                    "solves" => {
-                        let value = value.ok_or_else(|| ConfigEditError::MissingValue {
-                            key: key.to_string(),
-                        })?;
-                        self.concurrency.solves =
-                            value
-                                .parse()
-                                .map_err(|e| ConfigEditError::NumberParseError {
-                                    key: key.to_string(),
-                                    source: e,
-                                })?;
-                    }
-                    "downloads" => {
-                        let value = value.ok_or_else(|| ConfigEditError::MissingValue {
-                            key: key.to_string(),
-                        })?;
-                        self.concurrency.downloads =
-                            value
-                                .parse()
-                                .map_err(|e| ConfigEditError::NumberParseError {
-                                    key: key.to_string(),
-                                    source: e,
-                                })?;
-                    }
-                    _ => {
-                        return Err(ConfigEditError::UnknownKey {
-                            key: key.to_string(),
-                            supported_keys: get_supported_keys(self),
-                        })
-                    }
-                }
-                Ok(())
-            }
-            key if key.starts_with("run-post-link-scripts") => {
-                let value = value.ok_or_else(|| ConfigEditError::MissingValue {
-                    key: key.to_string(),
-                })?;
-                self.run_post_link_scripts =
-                    Some(value.parse().map_err(|e| ConfigEditError::InvalidValue {
-                        key: key.to_string(),
-                        source: Box::new(e),
-                    })?);
+                self.concurrency.set(key, value)?;
                 Ok(())
             }
             key if key.starts_with("proxy-config") => {
