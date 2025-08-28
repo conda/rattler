476d2529d00cd6772a1de4a96cddebbb654837e6
Bas Zalmstra <4995967+baszalmstra@users.noreply.github.com>
2025-08-28 11:46:00 +0200

    test: use pixi for e2e minio test

--- crates/rattler-bin/src/commands/auth.rs
+++ crates/rattler-bin/src/commands/auth.rs
@@ -1,7 +1,8 @@
+use miette::IntoDiagnostic;
 use rattler::cli::auth;
 
 pub type Opt = auth::Args;
 
 pub async fn auth(opt: Opt) -> miette::Result<()> {
-    auth::execute(opt).await.map_err(|e| anyhow::anyhow!(e))
+    auth::execute(opt).await.into_diagnostic()
 }
