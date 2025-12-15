f67db57c480b22ef747ab0a000887280852b800c
Bas Zalmstra <4995967+baszalmstra@users.noreply.github.com>
2025-12-15 09:13:47 +0100

    Update index.rs

--- crates/rattler_conda_types/src/package/index.rs
+++ crates/rattler_conda_types/src/package/index.rs
@@ -35,7 +35,7 @@ pub struct IndexJson {
     pub constrains: Vec<String>,
 
     /// The dependencies of the package
-    #[serde(default]
+    #[serde(default)]
     pub depends: Vec<String>,
 
     /// Extra dependency groups that can be selected using `foobar[extras=["scientific"]]`
