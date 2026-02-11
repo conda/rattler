023d4e77b07f3ba569e39e63ad01727946df3950
Dagmar Dinjens <s1151525@student.hsleiden.nl>
2026-02-11 14:56:59 +0100

    formatting in the py-rattler paths_json

--- py-rattler/src/paths_json.rs
+++ py-rattler/src/paths_json.rs
@@ -209,7 +209,7 @@ impl PyPathsEntry {
         prefix_placeholder: Option<PyPrefixPlaceholder>,
         sha256: Option<Bound<'_, PyBytes>>,
         size_in_bytes: Option<u64>,
-        executable: Option<bool>
+        executable: Option<bool>,
     ) -> PyResult<Self> {
         let sha256 = sha256.map(sha256_from_pybytes).transpose()?;
         Ok(Self {
@@ -220,7 +220,7 @@ impl PyPathsEntry {
                 prefix_placeholder: prefix_placeholder.map(Into::into),
                 sha256,
                 size_in_bytes,
-                executable
+                executable,
             },
         })
     }
@@ -392,7 +392,11 @@ impl From<PyPrefixPlaceholder> for PrefixPlaceholder {
 impl PyPrefixPlaceholder {
     /// Constructor
     #[new]
-    pub fn new(file_mode: PyFileMode, placeholder: &str, offsets: Option<Vec<usize>>) -> PyResult<Self> {
+    pub fn new(
+        file_mode: PyFileMode,
+        placeholder: &str,
+        offsets: Option<Vec<usize>>,
+    ) -> PyResult<Self> {
         Ok(Self {
             inner: PrefixPlaceholder {
                 file_mode: file_mode.into(),
