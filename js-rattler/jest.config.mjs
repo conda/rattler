2928e35e4bf4286595d75396bf63ae6a19f819ab
suhr25 <suhridmarwah07@gmail.com>
2026-02-24 17:15:50 +0000

    test(js): increase Jest testTimeout to 60s for network-dependent solve tests
    Signed-off-by: suhr25 <suhridmarwah07@gmail.com>

--- js-rattler/jest.config.mjs
+++ js-rattler/jest.config.mjs
@@ -2,4 +2,7 @@ import { createDefaultEsmPreset } from "ts-jest";
 
 export default {
     ...createDefaultEsmPreset(),
+    // Solve tests fetch live repodata over the network; 5 s (Jest default) is
+    // too tight on CI runners with variable latency.
+    testTimeout: 60_000,
 };
