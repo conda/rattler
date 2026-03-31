b3aaf20de300113e3394146e534344ba8072fec4
Pavel Zwerschke <pavelzw@gmail.com>
2026-03-31 17:18:38 +0200

    no exclude-newer in rattler_lock

--- crates/rattler_lock/src/channel.rs
+++ crates/rattler_lock/src/channel.rs
@@ -1,4 +1,3 @@
-use crate::ExcludeNewer;
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 
@@ -8,10 +7,6 @@ use serde_with::serde_as;
 pub struct Channel {
     /// The URL of the channel. File paths are also supported.
     pub url: String,
-    /// A channel-specific exclude-newer override.
-    #[serde(rename = "exclude-newer")]
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub exclude_newer: Option<ExcludeNewer>,
     /// Used env vars for the channel (e.g. hints for passwords or other secrets)
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     #[serde_as(as = "crate::utils::serde::Ordered<_>")]
@@ -22,7 +17,6 @@ impl From<String> for Channel {
     fn from(url: String) -> Self {
         Self {
             url,
-            exclude_newer: None,
             used_env_vars: Vec::default(),
         }
     }
@@ -32,7 +26,6 @@ impl From<&str> for Channel {
     fn from(url: &str) -> Self {
         Self {
             url: url.to_string(),
-            exclude_newer: None,
             used_env_vars: Vec::default(),
         }
     }
--- crates/rattler_lock/src/options.rs
+++ crates/rattler_lock/src/options.rs
@@ -1,83 +1,7 @@
 //! Defines [`SolveOptions`] and reexports from `rattler_solve` that are used.
 
-use std::collections::BTreeMap;
-
-use chrono::{DateTime, Utc};
-use rattler_conda_types::PackageName;
-
 // Reexport these fields.
 pub use rattler_solve::{ChannelPriority, SolveStrategy};
-
-/// Specifies how package candidates newer than a cutoff should be excluded.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum ExcludeNewer {
-    /// An absolute cutoff timestamp.
-    Timestamp(DateTime<Utc>),
-    /// A relative duration cutoff, resolved against the current time.
-    Duration(std::time::Duration),
-}
-
-impl From<DateTime<Utc>> for ExcludeNewer {
-    fn from(value: DateTime<Utc>) -> Self {
-        Self::Timestamp(value)
-    }
-}
-
-impl From<std::time::Duration> for ExcludeNewer {
-    fn from(value: std::time::Duration) -> Self {
-        Self::Duration(value)
-    }
-}
-
-impl From<ExcludeNewer> for rattler_solve::ExcludeNewer {
-    fn from(value: ExcludeNewer) -> Self {
-        match value {
-            ExcludeNewer::Timestamp(cutoff) => Self::from_datetime(cutoff),
-            ExcludeNewer::Duration(duration) => Self::from_duration(duration),
-        }
-    }
-}
-
-impl From<&ExcludeNewer> for rattler_solve::ExcludeNewer {
-    fn from(value: &ExcludeNewer) -> Self {
-        value.clone().into()
-    }
-}
-
-impl serde::Serialize for ExcludeNewer {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        match self {
-            Self::Timestamp(cutoff) => cutoff.serialize(serializer),
-            Self::Duration(duration) => {
-                serializer.serialize_str(&humantime::format_duration(*duration).to_string())
-            }
-        }
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for ExcludeNewer {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        #[derive(serde::Deserialize)]
-        #[serde(untagged)]
-        enum RawExcludeNewer {
-            Timestamp(DateTime<Utc>),
-            Duration(String),
-        }
-
-        match RawExcludeNewer::deserialize(deserializer)? {
-            RawExcludeNewer::Timestamp(cutoff) => Ok(ExcludeNewer::Timestamp(cutoff)),
-            RawExcludeNewer::Duration(duration) => humantime::parse_duration(&duration)
-                .map(ExcludeNewer::Duration)
-                .map_err(serde::de::Error::custom),
-        }
-    }
-}
 
 /// The prerelease mode used to resolve `PyPI` dependencies.
 ///
@@ -115,39 +39,7 @@ pub struct SolveOptions {
     #[serde(default, skip_serializing_if = "crate::utils::serde::is_default")]
     pub channel_priority: ChannelPriority,
 
-    /// Packages after this date have been excluded from the lock file.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub exclude_newer: Option<ExcludeNewer>,
-
-    /// Package-specific exclude-newer overrides.
-    #[serde(default, skip_serializing_if = "BTreeMap::is_empty")]
-    pub package_exclude_newer: BTreeMap<PackageName, ExcludeNewer>,
-
     /// The prerelease mode that was used to resolve `PyPI` dependencies.
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub pypi_prerelease_mode: Option<PypiPrereleaseMode>,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::ExcludeNewer;
-
-    #[test]
-    fn serializes_duration_with_short_unit_suffixes() {
-        let value = serde_yaml::to_string(&ExcludeNewer::Duration(std::time::Duration::from_secs(
-            3 * 24 * 60 * 60,
-        )))
-        .unwrap();
-
-        assert_eq!(value.trim(), "3days");
-    }
-
-    #[test]
-    fn deserializes_duration_strings() {
-        let value: ExcludeNewer = serde_yaml::from_str("3days").unwrap();
-        assert_eq!(
-            value,
-            ExcludeNewer::Duration(std::time::Duration::from_secs(3 * 24 * 60 * 60))
-        );
-    }
 }
