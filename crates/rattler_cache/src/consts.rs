f97551b85ac74b759a91bcaa33a54e2d901574db
Tobias Hunger <tobias.hunger@prefix.dev>
2026-08-18 14:55:13 +0200

    feat(rattler-cache): implement virtual package plugins
    ... in the rattler-cache layer.
    
    All changes are behind the `experimental-virtual-package-plugins` feature
    flag.

--- crates/rattler_cache/src/consts.rs
+++ crates/rattler_cache/src/consts.rs
@@ -4,3 +4,6 @@ pub const RUN_EXPORTS_CACHE_DIR: &str = "run-exports";
 /// The location in the main cache folder where the repodata cache is stored.
 pub const REPODATA_CACHE_DIR: &str = "repodata";
 pub const EXEC_ENVS_DIR: &str = "exec";
+/// The location where virtual package detection plugin results are stored.
+#[cfg(feature = "experimental-virtual-package-plugins")]
+pub const VIRTUAL_PACKAGE_PLUGINS_CACHE_DIR: &str = "virtual-package-plugins";
