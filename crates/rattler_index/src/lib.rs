0b1bc65cf8bddcefda1f3ea86d8305809c39241d
Tobias Hunger <tobias.hunger@prefix.dev>
2026-08-18 14:55:13 +0200

    feat(rattler-index): implement virtual package plugins
    ... in the rattler-index layer.
    
    All changes are behind the `experimental-virtual-package-plugins` feature
    flag.

--- crates/rattler_index/src/lib.rs
+++ crates/rattler_index/src/lib.rs
@@ -28,6 +28,8 @@ use opendal::layers::RetryLayer;
 #[cfg(feature = "s3")]
 use opendal::services::S3Config;
 use opendal::{Configurator, Operator, services::FsConfig};
+#[cfg(feature = "experimental-virtual-package-plugins")]
+use rattler_conda_types::VirtualPackagePlugins;
 use rattler_conda_types::{
     ChannelInfo, ChannelNotice, ChannelNotices, ChannelRelations, PackageRecord, PatchInstructions,
     Platform, RepoData, Shard, ShardedRepodata, ShardedSubdirInfo, UrlOrPath, V3Extensions,
@@ -951,7 +953,7 @@ async fn index_subdir_inner(
             ),
             channel_relations: channel_metadata.channel_relations,
             #[cfg(feature = "experimental-virtual-package-plugins")]
-            virtual_package_plugins: rattler_conda_types::VirtualPackagePlugins::default(),
+            virtual_package_plugins: VirtualPackagePlugins::default(),
         }),
         packages,
         conda_packages,
@@ -1411,7 +1413,7 @@ pub async fn write_repodata(
                 repodata_revisions: sharded_repodata_revisions,
                 channel_relations: sharded_channel_relations,
                 #[cfg(feature = "experimental-virtual-package-plugins")]
-                virtual_package_plugins: rattler_conda_types::VirtualPackagePlugins::default(),
+                virtual_package_plugins: VirtualPackagePlugins::default(),
             },
             shards: shards
                 .iter()
@@ -1896,7 +1898,7 @@ pub async fn ensure_channel_initialized_with_channel_metadata(
             repodata_revisions: RepodataRevisions::new(),
             channel_relations: channel_metadata.channel_relations,
             #[cfg(feature = "experimental-virtual-package-plugins")]
-            virtual_package_plugins: rattler_conda_types::VirtualPackagePlugins::default(),
+            virtual_package_plugins: VirtualPackagePlugins::default(),
         }),
         packages: IndexMap::default(),
         conda_packages: IndexMap::default(),
